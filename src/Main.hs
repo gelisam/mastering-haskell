b02642ba08c6cb43a376282e06ca1362bdbab56c
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-23 17:13:16 -0400

    new slide

---
+++ src/Main.hs
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+        -------------------------------------------------
+        --                                             --
+        --  Manual Parallelism using                   --
+        --  the Free Applicative                       --
+        --                         by Samuel Gélineau  --
+        --                         published by Packt  --
+        --                                             --
+        -------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main :: IO ()
+main = putStrLn "Welcome to the course!"
