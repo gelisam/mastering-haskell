b619f4edcf3e011d873a876b74a7cc2afd5502f3
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-23 17:13:16 -0400

    new slide

---
+++ src/Main.hs
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+        -------------------------------------------------
+        --                                             --
+        --  Transforming Effectful Streams             --
+        --  using the Free Monad                       --
+        --                         by Samuel Gélineau  --
+        --                         published by Packt  --
+        --                                             --
+        -------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main :: IO ()
+main = putStrLn "Welcome to the course!"
