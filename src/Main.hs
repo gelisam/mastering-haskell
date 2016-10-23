451ea7a10f54673bf35273c4b65d96540d029493
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-23 17:13:16 -0400

    new slide

---
+++ src/Main.hs
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+                            -------------------------------------------------
+                            --                                             --
+                            --  Fewer Sins using the Free Monad            --
+                            --                                             --
+                            --                         by Samuel Gélineau  --
+                            --                         published by Packt  --
+                            -------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main :: IO ()
+main = putStrLn "Welcome to the course!"
