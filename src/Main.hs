2ff7d17274b61f8a677220aa348f47d06a9526a3
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-23 17:13:16 -0400

    new slide

---
+++ src/Main.hs
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+        -------------------------------------------------
+        --                                             --
+        --  Who's Driving? Push, Pull… or Both!        --
+        --                                             --
+        --                         by Samuel Gélineau  --
+        --                         published by Packt  --
+        --                                             --
+        -------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main :: IO ()
+main = putStrLn "Welcome to the course!"
