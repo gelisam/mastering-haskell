efddcd71441cc90bf9bfaaa4148b06b94e1203f1
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-23 17:13:16 -0400

    new slide

---
+++ src/Main.hs
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+        -------------------------------------------------
+        --                                             --
+        --  Composing Finite and Infinite Streams      --
+        --                                             --
+        --                         by Samuel Gélineau  --
+        --                         published by Packt  --
+        --                                             --
+        -------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main :: IO ()
+main = putStrLn "Welcome to the course!"
