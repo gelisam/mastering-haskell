42ca2fcd57b6fa0074cb8111262bcd58e37c230f
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-23 17:13:16 -0400

    new slide

---
+++ src/Main.hs
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+        -------------------------------------------------
+        --                                             --
+        --        Mastering Haskell Programming        --
+        --                                             --
+        --                         by Samuel Gélineau  --
+        --                         published by Packt  --
+        --                                             --
+        -------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main :: IO ()
+main = putStrLn "Welcome to the course!"
