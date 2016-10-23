0e7e85608f47a2b4cfb7c9d0ebfe133931ccbc75
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-23 17:13:16 -0400

    new slide

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+stack install
+
+time ./scripts/run_tests_and_hang.sh "$@" &
+PID="$!"
+trap "./scripts/kill_recursively.sh $PID" EXIT
+
+
+# wait for the child process to terminate
+wait
