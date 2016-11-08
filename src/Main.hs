cdbceffe9ff7d0442532d1b26e2f0a79fb57344c
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-08 05:52:51 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import Text.Printf
+
+
+fib :: Int -> Integer
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
 
 main :: IO ()
-main = putStrLn "hello world"
+main = printf "fib 10 = %d" (fib 10)
