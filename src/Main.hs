ce3475d4093185285eab60fe5327294cf265465d
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-20 18:22:31 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -4,18 +4,16 @@ data Statement = PutStrLn String deriving Show
 type Logging = [Statement]
 
 -- |
--- >>> runStatement $ PutStrLn "hello"
--- hello
-runStatement :: Statement -> IO ()
-runStatement (PutStrLn s) = putStrLn s
+-- >>> testStatement $ PutStrLn "hello"
+-- ["hello"]
+testStatement :: Statement -> [String]
+testStatement (PutStrLn s) = [s]
 
 -- |
--- >>> runLogging $ replicate 3 (PutStrLn "hello")
--- hello
--- hello
--- hello
-runLogging :: Logging -> IO ()
-runLogging = mapM_ runStatement
+-- >>> testLogging $ replicate 3 (PutStrLn "hello")
+-- ["hello","hello","hello"]
+testLogging :: Logging -> [String]
+testLogging = foldMap testStatement
 
 
 
