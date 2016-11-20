cbc8ef74dfb00fc8722c7c3582a1fbdb0a088466
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-20 13:15:45 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,6 @@
 module Main where
 
-putStrLn :: String -> IO ()
-getLine  :: IO String
-
-readFile  :: FilePath -> IO String
-writeFile :: FilePath -> String -> IO ()
-
-newIORef   :: a -> IO (IORef a)
-readIORef  :: IORef a -> IO a
-writeIORef :: IORef a -> a -> IO ()
-
-forkIO :: IO () -> IO ThreadId
-killThread :: ThreadId -> IO ()
-
-threadDelay :: Int -> IO ()
-
-try :: Exception e => IO a -> IO (Either e a)
-finally :: IO a -> IO b -> IO a
-
-newUnique :: IO Unique
-makeStableName :: a -> IO (StableName a)
-
-getArgs :: IO [String]
-getEnv :: String -> IO String
-exitSuccess :: IO a
-exitFailure :: IO a
-
-newCString :: String -> IO CString
-peekCString :: CString -> IO String
-
-performGC :: IO ()
+putStrLn :: String -> Console ()
+getLine  :: Console String
 
 
