9689a819075a9699c753db67b56e5c5db53c034c
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-20 21:35:23 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,119 +1,147 @@
 module Main where
 
+import Prelude hiding (getLine, putStrLn)
+
+instance Monad Console where
+  return = Return
+  Return x >>= f = f x
+  More cc  >>= f = More (fmap (>>= f) cc)
+
+
+getLineLength :: Console Int
+getLineLength = do
+    line <- getLine
+    return (length line)
+
+echo :: Console ()
+echo = do
+    line <- getLine
+    putStrLn line
+
+getLine :: Console String
+getLine = More (GetLine Return)
+
+putStrLn :: String -> Console ()
+putStrLn s = More (PutStrLn s Return)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 data ConsoleF a
   = GetLine         (String -> a)
   | PutStrLn String (()     -> a)
+
+instance Functor ConsoleF where
+  fmap f (GetLine    cc) = GetLine    (fmap f cc)
+  fmap f (PutStrLn s cc) = PutStrLn s (fmap f cc)
+
 
 data Console a
   = Return a
   | More (ConsoleF (Console a))
 
-getLineLength :: Console Int
-getLineLength =
-    More $ GetLine $ \line ->
-    Return (length line)
-
-echo :: Console ()
-echo =
-    More $ GetLine $ \line ->
-    More $ PutStrLn line $ \() ->
-    Return ()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+instance Functor Console where
+  fmap f (Return x) = Return (f x)
+  fmap f (More cc)  = More (fmap (fmap f) cc)
+
+instance Applicative Console where
+  pure = Return
+  cf <*> cx = do
+    f <- cf
+    x <- cx
+    return (f x)
 
 
 
