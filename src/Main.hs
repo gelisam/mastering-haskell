178cdcbfa6595b465457e096be132d57ae12bbc7
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-24 11:17:10 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,119 @@
 module Main where
 import Control.Arrow
+import Control.Concurrent
 import Control.Exception.Base
+import Control.Monad
 import Data.Typeable
+import System.Timeout
+
+
+-- |
+-- >>> timeout 1000000 sleepForever
+-- Nothing 
+sleepForever :: IO a
+sleepForever = forever
+             $ threadDelay 2000000
+
+-- |
+-- >>> timeout 1000000 uninterruptibleSleep
+-- ...
+uninterruptibleSleep :: IO a
+uninterruptibleSleep = forever
+                     $ catchType $ threadDelay 2000000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 typeOfException :: SomeException -> TypeRep
@@ -11,117 +123,9 @@ catchType :: IO a -> IO (Either TypeRep a)
 catchType = fmap (left typeOfException) . try
 
 
--- |
--- >>> try example :: IO (Either ArithException Int)
--- *** Exception: oops
--- 
--- >>> catchType example
--- Left AssertionFailed
--- 
--- >>> try example :: IO (Either AssertionFailed Int)
--- Left oops
-example :: IO Int
-example = throwIO $ AssertionFailed "oops"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- prevent a warning about unsued System.Timeout
+timeout' :: Int -> IO a -> IO (Maybe a)
+timeout' = timeout
 
 
 main :: IO ()
