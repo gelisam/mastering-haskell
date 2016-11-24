8d1208f970975f59195691532738c2a5f15b9f65
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-24 13:12:55 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,23 @@
 module Main where
-import Control.Concurrent
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Resource
-import System.IO
-import System.Timeout
-
-acquireFile :: FilePath -> IOMode -> ResIO (ReleaseKey, Handle)
-acquireFile filePath mode = allocate open close
-  where
-    open = liftIO $ openFile filePath mode
-    close h = liftIO $ do
-      hClose h
-      putStrLn "closed the file."
+import Control.Arrow
+import Control.Exception.Base
+import Data.Typeable
 
 -- |
--- >>> timeout 1000000 example
--- closed the file.
--- Nothing
-example :: IO a
-example = runResourceT $ do
-  (releaseKey, h) <- acquireFile "foo.txt" ReadMode
-  liftIO sleepForever
+-- >>> example
+-- *** Exception: oops
+-- 
+-- >>> Right x <- catchType $ return example
+-- >>> print x
+-- *** Exception: oops
+-- 
+-- >>> catchType $ example `seq` return 42
+-- Left AssertionFailed
+-- 
+-- >>> catchType $ evaluate example
+-- Left AssertionFailed
+example :: Int
+example = 1 + throw (AssertionFailed "oops")
 
 
 
@@ -113,14 +109,21 @@ example = runResourceT $ do
 
 
 
-sleepForever :: IO a
-sleepForever = forever
-             $ threadDelay 2000000
 
 
--- prevent a warning about unsued System.Timeout
-timeout' :: Int -> IO a -> IO (Maybe a)
-timeout' = timeout
+
+
+
+
+
+
+typeOfException :: SomeException -> TypeRep
+typeOfException (SomeException e) = typeOf e
+
+catchType :: IO a -> IO (Either TypeRep a)
+catchType = fmap (left typeOfException) . try
+
+
 
 
 main :: IO ()
