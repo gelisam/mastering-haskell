db7b6ffa7fec591569ee551830a41cbfda5ab814
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-25 14:54:50 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,13 @@
 {-# LANGUAGE BangPatterns #-}
 module Main where
-import System.IO
+
 
 main :: IO ()
 main = do
   writeFile "foo.txt" $ unlines $ map show [1..10000::Int]
   
-  h <- openFile "foo.txt" ReadMode
-  let go :: Int -> Int -> IO Int
-      go 100 !acc = return acc
-      go n   !acc = do
-        r <- hIsEOF h
-        if r then return acc
-             else do x <- read <$> hGetLine h
-                     go (n+1) (if odd x then x+acc else acc)
-  r <- go 0 0
-  print r
+  s <- readFile "foo.txt"
+  print (transform s)
+
+transform :: String -> Int
+transform = sum . filter odd . take 100 . map read . lines
