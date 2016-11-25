d6490ce78d56c43f9fc577be68bc1f400a11abd8
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-24 19:35:37 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,60 +1,23 @@
+module Main where
+import System.IO.Unsafe
+import Text.Printf
 
 
+verboseSum :: Int -> Int -> Int
+verboseSum x y = unsafePerformIO $ do
+  printf "adding %d and %d\n" x y
+  return (x + y)
 
-
-
-
-
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  Simple Streaming using Lazy IO             --
-        --                                             --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        -------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+generateVerboseProduct :: Int -> Int -> IO Int
+generateVerboseProduct x y = unsafeInterleaveIO $ do
+  printf "multiplying %d and %d\n" x y
+  return (x * y)
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = do
+  let x1 = verboseSum 6 7
+  let x2 = verboseSum 6 7
+  x3 <- generateVerboseProduct 6 7
+  x4 <- generateVerboseProduct 6 7
+  putStrLn "about to print"
+  print [x1, x2, x3, x4]
