c890c143d3bb73e617199aca1664ff75039d7de2
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-25 11:55:13 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,23 @@
 module Main where
-
+import System.IO.Unsafe
 source :: IO [Int]
 source = go 1
   where
     go 4 = return []
     go x = do putStrLn $ "source " ++ show x
-              xs <- go (x+1)
+              xs <- unsafeInterleaveIO $ go (x+1)
               return (x:xs)
 
 odds :: [Int] -> IO [Int]
 odds []     = return []
 odds (x:xs) = do putStrLn $ "filter " ++ show x
-                 xs' <- odds xs
+                 xs' <- unsafeInterleaveIO $ odds xs
                  return $ if odd x then x:xs' else xs'
 
 sink :: [Int] -> IO Int
 sink []     = return 0
 sink (x:xs) = do putStrLn $ "sink " ++ show x
-                 (x +) <$> sink xs
+                 (x +) <$> unsafeInterleaveIO (sink xs)
 
 main :: IO ()
 main = do r <- source >>= odds >>= sink
