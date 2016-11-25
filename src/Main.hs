0f93a0b69a53051c45e3f5c57bdcc6f16e672549
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-25 11:39:52 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,24 @@
 module Main where
-import Data.Function
-import Debug.Trace
 
-source :: [Int]
-source = map (\x -> trace ("source " ++ show x) x) [1..3]
+source :: IO [Int]
+source = go 1
+  where
+    go 4 = return []
+    go x = do putStrLn $ "source " ++ show x
+              xs <- go (x+1)
+              return (x:xs)
 
-odds :: [Int] -> [Int]
-odds = filter (\x -> trace ("filter " ++ show x) $ odd x)
+odds :: [Int] -> IO [Int]
+odds []     = return []
+odds (x:xs) = do putStrLn $ "filter " ++ show x
+                 xs' <- odds xs
+                 return $ if odd x then x:xs' else xs'
 
-sink :: [Int] -> Int
-sink []     = 0
-sink (x:xs) = trace ("sink " ++ show x) (x + sink xs)
+sink :: [Int] -> IO Int
+sink []     = return 0
+sink (x:xs) = do putStrLn $ "sink " ++ show x
+                 (x +) <$> sink xs
 
 main :: IO ()
-main = do
-  let r = source & odds & sink
-  print r
+main = do r <- source >>= odds >>= sink
+          print r
