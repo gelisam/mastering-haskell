366a67bd0f5c39a402d6bc50c59c108804d914bf
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-26 12:51:47 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,43 @@
 module Main where
+import Control.Applicative
 import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.List
 import Control.Monad.Trans.State
 
-pytagoreanTriples :: [(Int,Int,Int)]
+pytagoreanTriples :: ListT (State Bool) (Int,Int,Int)
 pytagoreanTriples = do
-  x <- [1..10]
-  y <- [x..10]
-  z <- [y..10]
+  x <- oneOf [1..10]
+  y <- oneOf [x..10]
+  z <- oneOf [y..10]
   guard (x*x + y*y == z*z)
+  _ <- lift toggle
   return (x,y,z)
+
+
+main :: IO ()
+main = do
+  let (xs,flag) = runState (runListT pytagoreanTriples) False
+  putStrLn $ "found an " ++ if flag then "odd" else "even"
+                         ++ " number of triples:"
+  print xs
+
+
+
+
+
+
+
+
+
+-- how does this not already exist??
+oneOf :: Monad m => [a] -> ListT m a
+oneOf []     = empty
+oneOf (x:xs) = return x <|> oneOf xs
+
 
 toggle :: Monad m => StateT Bool m Bool
 toggle = do
   newState <- not <$> get
   put newState
   return newState
-
-
-main :: IO ()
-main = do
-  print pytagoreanTriples
-  print [execState (replicateM_ n toggle) False | n <- [0..4]]
-
-
-
-
-
-
-
-
-
-
-
-
-
