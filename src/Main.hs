9daaa1a622eb4ef1e0125df6fdd0ad0b55ef4674
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-26 14:10:20 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,60 +1,36 @@
+module Main where
+import Control.Monad
+import Control.Monad.Trans.State
 
+pytagoreanTriples :: [(Int,Int,Int)]
+pytagoreanTriples = do
+  x <- [1..10]
+  y <- [x..10]
+  z <- [y..10]
+  guard (x*x + y*y == z*z)
+  return (x,y,z)
 
-
-
-
-
-
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  IO as a "Sin Bin"                          --
-        --                                             --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        -------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+toggle :: Monad m => StateT Bool m Bool
+toggle = do
+  newState <- not <$> get
+  put newState
+  return newState
 
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = do
+  print pytagoreanTriples
+  print [execState (replicateM_ n toggle) False | n <- [0..4]]
+
+
+
+
+
+
+
+
+
+
+
+
+
