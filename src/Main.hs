236a4a5f54801c6d5caa3fcce3a6546d2c31fe98
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-27 12:10:52 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -3,22 +3,22 @@ import Control.Concurrent as Concurrent
 import Foreign
 import System.Mem
 import System.Mem.Weak
-
+import Data.IORef
 main :: IO ()
 main = do
-  ptr@(Ptr addr) <- mallocBytes 1024
+  ptr <- mallocBytes 1024
   let freePtr = do putStrLn $ "freeing " ++ show ptr
                    free ptr
-  weak <- mkWeakPtr ptr (Just freePtr)
-  
+  ref <- newIORef ptr
+  weak <- mkWeakIORef ref freePtr
   
   performGC >> Concurrent.yield
   
   putStrLn $ "still referenced"
-  deRefWeak weak >>= print
-  pokeAddr addr (42 :: Int)
+  deRefWeak weak >>= traverse readIORef >>= print
+  poke ptr (42 :: Int) >> modifyIORef ref id
   
   performGC >> Concurrent.yield
   
   putStrLn "no longer referenced"
-  deRefWeak weak >>= print
+  deRefWeak weak >>= traverse readIORef >>= print
