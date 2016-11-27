ac4b2a83b66312a1fb553358863d24dcea41fe28
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-26 23:30:39 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,58 +1,52 @@
 module Main where
 import Control.Monad
-import Data.Array.IO
-import Data.Vector.Mutable as Vector
+import Data.Array
+import qualified Data.Vector as V
 
-type SquareMatrix a = IOArray (Int,Int) a
+type SquareMatrix a = Array (Int,Int) a
 
-identityMatrix :: Num a => Int -> IO (SquareMatrix a)
-identityMatrix n = do
-  arr <- newArray ((1,1), (n,n)) 0
-  forM_ [1..n] $ \i -> do
-    writeArray arr (i,i) 1
-  return arr
+identityMatrix :: Num a => Int -> SquareMatrix a
+identityMatrix n = listArray ((1,1), (n,n)) (repeat 0)
+                // [ ((i,i), 1) | i <- [1..n] ]
 
 
-squares :: Num a => IO (IOVector a)
-squares = do
-  vect <- Vector.new 4
-  Vector.write vect 0 4
-  Vector.write vect 1 9
-  Vector.write vect 2 16
-  Vector.write vect 3 25
-  return vect
+
+
+
+squares :: Num a => V.Vector a
+squares = V.cons 4
+        $ V.cons 9
+        $ V.cons 16
+        $ V.cons 25
+        $ V.empty
+
+
+
+
+
+
+
+
+
 
 
 
 
 main :: IO ()
 main = do
-  arr <- identityMatrix 4
-  printSquareMatrix (arr :: SquareMatrix Int)
-  vect <- squares
-  printVector (vect :: IOVector Int)
-
-
-
-
-
-
+  printSquareMatrix (identityMatrix 4 :: SquareMatrix Int)
+  printVector (squares :: V.Vector Int)
 
 
 printSquareMatrix :: Show a => SquareMatrix a -> IO ()
 printSquareMatrix arr = do
-  ((i0,j0), (iZ,jZ)) <- getBounds arr
+  let ((i0,j0), (iZ,jZ)) = bounds arr
   forM_ [j0..jZ] $ \j -> do
     forM_ [i0..iZ] $ \i -> do
-      x <- readArray arr (i,j)
+      let x = arr ! (i,j)
       putStr (show x)
       putStr " "
     putStrLn ""
 
-printVector :: Show a => IOVector a -> IO ()
-printVector vect = go (Vector.length vect) []
-  where
-    go 0 acc = print acc
-    go i acc = do
-      x <- Vector.read vect (i-1)
-      go (i-1) (x:acc)
+printVector :: Show a => V.Vector a -> IO ()
+printVector = print
