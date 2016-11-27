405562951d38c5f3f91f60acfc79f4563acf3f7e
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-27 17:27:01 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,18 @@
 module Main where
-
+import Debug.Trace
 import System.Mem.StableName
 
 main :: IO ()
 main = do
-  let xs1 = [1..1000] :: [Int]
-  let xs2 = [1..500] ++ [501..1000] :: [Int]
+  let xs1 = trace "xs1" [1..1000] :: [Int]
+  let xs2 = trace "xs2" xs1
 
   name1 <- makeStableName xs1
   name2 <- makeStableName xs2
-  print (name1 == name1, name1 == name2)
+  print (name1 == name2)
   print (xs1 == xs1, xs1 == xs2)
+  
+  name1' <- makeStableName xs1
+  name2' <- makeStableName xs2
+  print (name1' == name2')
+  print (name1 == name1', name2 == name2')
