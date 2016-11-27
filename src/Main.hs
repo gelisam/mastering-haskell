569d5fb6edb92ecdde217fb4fade203db30d35ad
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-27 12:15:12 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,24 @@
 module Main where
 import Control.Concurrent as Concurrent
-import Foreign.Concurrent as Concurrent
 import Foreign
 import System.Mem
+import System.Mem.Weak
 
 main :: IO ()
 main = do
   ptr <- mallocBytes 1024
   let freePtr = do putStrLn $ "freeing " ++ show ptr
                    free ptr
-  foreignPtr <- Concurrent.newForeignPtr ptr freePtr
+  weak <- mkWeakPtr ptr (Just freePtr)
   
   
   performGC >> Concurrent.yield
   
   putStrLn $ "still referenced"
-  withForeignPtr foreignPtr $ flip poke (42 :: Int)
-  
+  deRefWeak weak >>= print
+  poke ptr (42 :: Int)
   
   performGC >> Concurrent.yield
   
   putStrLn "no longer referenced"
+  deRefWeak weak >>= print
