d777bd7205f457f223b1336b48796e63c138a277
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-28 18:22:21 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,14 @@
-{-# LANGUAGE BangPatterns #-}
+
 module Main where
 import Debug.Trace
 
-foo :: Num a => [a] -> a
+foo :: [Int] -> Int
 foo = go 0
   where
     go acc []     = acc
     go acc (x:xs) = do
-      let acc' = trace "add" $ acc + x
-      trace "recursive call" $ go acc' xs
+      let acc' = trace (show acc ++ " + " ++ show x) $ acc + x
+      trace ("go " ++ show acc' ++ " " ++ show xs) $ go acc' xs
 
 main :: IO ()
 main = print (foo [1..10] :: Int)
