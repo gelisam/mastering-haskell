2c83b06b98d8101fac5834f66b96546b04386c2d
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-28 17:35:27 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -6,9 +6,9 @@ foo :: Num a => [a] -> a
 foo = go 0
   where
     go acc []     = acc
-    go acc (x:xs) = go (acc+x) xs
-
-
+    go acc (x:xs) = do
+      let acc' = acc + x
+      go acc' xs
 
 main :: IO ()
 main = print (foo [1..10] :: Int)
