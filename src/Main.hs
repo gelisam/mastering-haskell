7cb1db21c8e5520bd97ca3a459778170de10c16b
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-28 18:20:08 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,13 @@
-{-# LANGUAGE Strict #-}
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
 
 foo :: Num a => [a] -> a
 foo = go 0
   where
-    go acc []     = acc
-    go acc (x:xs) = do
-      let acc' = acc + x
+    go !acc ![]     = acc
+    go !acc !(x:xs) = do
+      let !acc' = acc + x
       go acc' xs
 
 main :: IO ()
