e84540955b857447f04f75f9a005c912d30fd8bc
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-28 19:13:23 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE Strict #-}
+
 module Main where
 import Debug.Trace
 
 main :: IO ()
-main = trace "outer" $ trace "inner" $ return ()
+main = trace "outer" $! trace "inner" $! return ()
