bae4c8fd17901aa9f8ce0599026eed7220323d55
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-28 19:12:02 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,6 @@
-
+{-# LANGUAGE Strict #-}
 module Main where
 import Debug.Trace
 
-foo :: [Int] -> Int
-foo = go 0
-  where
-    go acc []     = acc
-    go acc (x:xs) = do
-      let acc' = trace (show acc ++ " + " ++ show x) $ acc + x
-      trace ("go " ++ show acc' ++ " " ++ show xs) $ go acc' xs
-
 main :: IO ()
-main = print (foo [1..10] :: Int)
+main = trace "outer" $ trace "inner" $ return ()
