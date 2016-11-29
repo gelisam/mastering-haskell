d0797d0e6f7b359bf073c4d05cf2743e478124e5
Samuel Gélineau <gelisam+github@gmail.com>
2016-11-29 13:58:58 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,38 @@
 module Main where
 
+add100 :: Int -> Int
+add100 = (+100)
+
+transform1 :: [Int] -> [Int]
+transform1 = map add100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 source :: [Int]
 source = [0..]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 sink :: Show a => [a] -> IO ()
 sink xs = do
   putStrLn ""
-  mapM_ print (take 10 xs)
+  mapM_ print (take 5 xs)
   putStrLn "..."
 
 main :: IO ()
-main = sink source
+main = sink (transform1 source)
