94fec15e54f2f26503a0a85a1a0b100f284f5cf2
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-01 13:29:28 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,64 @@
 module Main where
 
+data TransformF u v a = Consume   (u  -> a)
+                      | Produce v (() -> a)
+
+data FTransform u v a = ReturnF a
+                      | MoreF (TransformF u v (FTransform u v a))
+data ITransform u v   = MoreI (TransformF u v (ITransform u v))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 type FList a = [a]
 data IList a = Cons a (IList a)
 
-appendF :: FList a -> FList a -> FList a
-appendF []     xs' = xs'
-appendF (x:xs) xs' = x : appendF xs xs'
 
 
 
+instance Functor (TransformF u v) where
+  fmap f (Consume   cc) = Consume   (fmap f cc)
+  fmap f (Produce v cc) = Produce v (fmap f cc)
 
 
 
+consume :: FTransform u v u
+consume = MoreF (Consume ReturnF)
+
+produce :: v -> FTransform u v ()
+produce v = MoreF (Produce v ReturnF)
 
 
+instance Functor (FTransform u v) where
+  fmap f (ReturnF x) = ReturnF (f x)
+  fmap f (MoreF cc)  = MoreF (fmap (fmap f) cc)
 
+instance Applicative (FTransform u v) where
+  pure = ReturnF
+  cf <*> cx = do
+    f <- cf
+    x <- cx
+    return (f x)
 
-
-
-
-
-
-
-
-
+instance Monad (FTransform u v) where
+  return = ReturnF
+  ReturnF x >>= f = f x
+  MoreF cc  >>= f = MoreF (fmap (>>= f) cc)
 
 
 main :: IO ()
