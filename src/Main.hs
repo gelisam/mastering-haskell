4c34f9cd79918e26957c592c82e9aab65ba40044
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-01 13:29:12 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,6 @@ appendF :: FList a -> FList a -> FList a
 appendF []     xs' = xs'
 appendF (x:xs) xs' = x : appendF xs xs'
 
-appendI :: IList a -> IList a -> IList a
-appendI (Cons x xs) xs' = x `Cons` appendI xs xs'
 
 
 
