4c43f80e4901be4d1325da3060ca716a41f06704
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-01 13:29:12 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -3,14 +3,13 @@ module Main where
 type FList a = [a]
 data IList a = Cons a (IList a)
 
-filterF :: (a -> Bool) -> FList a -> FList a
-filterF _ []     = []
-filterF p (x:xs) | p x       = x : filterF p xs
-                 | otherwise =     filterF p xs
+zipF :: FList a -> FList b -> FList (a,b)
+zipF []     _      = []
+zipF _      []     = []
+zipF (x:xs) (y:ys) = (x,y) : zipF xs ys
 
-filterI :: (a -> Bool) -> IList a -> IList a
-filterI p (Cons x xs) | p x       = x `Cons` filterI p xs
-                      | otherwise =          filterI p xs
+zipI :: IList a -> IList b -> IList (a,b)
+zipI (Cons x xs) (Cons y ys) = (x,y) `Cons` zipI xs ys
 
 
 
