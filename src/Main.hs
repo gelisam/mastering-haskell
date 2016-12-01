b45e04ddd8fc3eedbd7ac647240abe1f9341c88b
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-01 13:43:15 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,5 @@
 module Main where
 
-instance Monad (FTransform u v) where
-  return = ReturnF
-  ReturnF x >>= f = f x
-  MoreF cc  >>= f = MoreF (fmap (>>= f) cc)
-
-appendI :: ITransform u v -> ITransform u v -> ITransform u v
-appendI (MoreI (Consume   cc1)) cc2 = MoreI $ Consume $ \u ->
-                                      appendI (cc1 u) cc2
-appendI (MoreI (Produce v cc1)) cc2 = MoreI $ Produce v $ \() ->
-                                      appendI (cc1 ()) cc2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -40,18 +11,10 @@ data FTransform u v a = ReturnF a
 data ITransform u v   = MoreI (TransformF u v (ITransform u v))
 
 
-runFTransform :: FTransform u v a -> IList u -> (FList v, a)
-runFTransform (ReturnF x)            _           = ([], x)
-runFTransform (MoreF (Consume   cc)) (Cons u us) = (vs, x)
-  where (vs, x) = runFTransform (cc u) us
-runFTransform (MoreF (Produce v cc)) us          = (v:vs, x)
-  where (vs, x) = runFTransform (cc ()) us
 
-runITransform :: ITransform u v -> IList u -> IList v
-runITransform (MoreI (Consume   cc)) (Cons u us) = vs
-  where vs = runITransform (cc u) us
-runITransform (MoreI (Produce v cc)) us          = v `Cons` vs
-  where vs = runITransform (cc ()) us
+
+
+
 
 
 
@@ -77,11 +40,11 @@ instance Functor (TransformF u v) where
 
 
 
-consume :: FTransform u v u
-consume = MoreF (Consume ReturnF)
+consumeF :: FTransform u v u
+consumeF = MoreF (Consume ReturnF)
 
-produce :: v -> FTransform u v ()
-produce v = MoreF (Produce v ReturnF)
+produceF :: v -> FTransform u v ()
+produceF v = MoreF (Produce v ReturnF)
 
 
 instance Functor (FTransform u v) where
@@ -95,6 +58,10 @@ instance Applicative (FTransform u v) where
     x <- cx
     return (f x)
 
+instance Monad (FTransform u v) where
+  return = ReturnF
+  ReturnF x >>= f = f x
+  MoreF cc  >>= f = MoreF (fmap (>>= f) cc)
 
 
 main :: IO ()
