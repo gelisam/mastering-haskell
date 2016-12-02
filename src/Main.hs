a60e2a3b0fa523c840b5a0bd9352fcc50df75427
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-02 09:14:41 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
-
-
+import Prelude hiding (id, (.))
+import Control.Category
 
 newtype PTransform u v = PTransform
   { onPush :: u
@@ -8,12 +8,12 @@ newtype PTransform u v = PTransform
            -> IO ()
   }
 
-
-
-
-
-
-
+instance Category PTransform where
+  id = PTransform $ \u pushU -> pushU u
+  t2 . t1 = PTransform $ \u pushW ->
+            onPush t1 u $ \v ->
+            onPush t2 v $ \w ->
+            pushW w
 
 
 
