15dbaad523a2a124d0baa74e42a3eb68c72af00b
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-02 09:09:43 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,11 @@ module Main where
 
 
 
-data TransformF u v a = Consume   (u  -> a)
-                      | Produce v (() -> a)
-
-
-data Transform u v a = Return a
-                     | More (TransformF u v (Transform u v a))
-
-
+newtype PTransform u v = PTransform
+  { onPush :: u
+           -> (v -> IO ())
+           -> IO ()
+  }
 
 
 
@@ -26,40 +23,10 @@ data Transform u v a = Return a
 
 
 
-type FList a = [a]
-data IList a = Cons a (IList a)
 
 
 
 
-instance Functor (TransformF u v) where
-  fmap f (Consume   cc) = Consume   (fmap f cc)
-  fmap f (Produce v cc) = Produce v (fmap f cc)
-
-
-
-consume :: Transform u v u
-consume = More (Consume Return)
-
-produce :: v -> Transform u v ()
-produce v = More (Produce v Return)
-
-
-instance Functor (Transform u v) where
-  fmap f (Return x) = Return (f x)
-  fmap f (More cc)  = More (fmap (fmap f) cc)
-
-instance Applicative (Transform u v) where
-  pure = Return
-  cf <*> cx = do
-    f <- cf
-    x <- cx
-    return (f x)
-
-instance Monad (Transform u v) where
-  return = Return
-  Return x >>= f = f x
-  More cc  >>= f = More (fmap (>>= f) cc)
 
 
 main :: IO ()
