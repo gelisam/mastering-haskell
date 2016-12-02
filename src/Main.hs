14db7d083ef3a1ead9a4cc3844630703cefa5178
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-02 09:24:28 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,33 @@
 module Main where
 import Prelude hiding (id, (.))
 import Control.Category
+import Control.Monad
+import Data.IORef
+
+repeatT :: Int -> PTransform u u
+repeatT n = PTransform $ \u pushU ->
+            replicateM_ n (pushU u)
+
+batchesOf :: Int -> IO (PTransform u [u])
+batchesOf n = do
+  ref <- newIORef []
+  return $ PTransform $ \u pushList -> do
+    modifyIORef ref (++ [u])
+    us <- readIORef ref
+    when (length us == n) $ do
+      writeIORef ref []
+      pushList us
+
+
+
+
+
+
+
+
+
+
+
 
 newtype PTransform u v = PTransform
   { onPush :: u
@@ -14,19 +41,6 @@ instance Category PTransform where
             onPush t1 u $ \v ->
             onPush t2 v $ \w ->
             pushW w
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 main :: IO ()
