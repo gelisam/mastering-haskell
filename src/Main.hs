afe13996b4780efbf263ca3cbf0fa2a6b4c93edf
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-01 22:27:53 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,13 @@
+module Main where
 
 
 
+data TransformF u v a = Consume   (u  -> a)
+                      | Produce v (() -> a)
 
 
-
-
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  Who's Driving? Push, Pull… or Both!        --
-        --                                             --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        --                                             --
-        -------------------------------------------------
-
-
-
+data Transform u v a = Return a
+                     | More (TransformF u v (Transform u v a))
 
 
 
@@ -37,25 +26,41 @@
 
 
 
+type FList a = [a]
+data IList a = Cons a (IList a)
 
 
 
 
+instance Functor (TransformF u v) where
+  fmap f (Consume   cc) = Consume   (fmap f cc)
+  fmap f (Produce v cc) = Produce v (fmap f cc)
 
 
 
+consume :: Transform u v u
+consume = More (Consume Return)
+
+produce :: v -> Transform u v ()
+produce v = More (Produce v Return)
 
 
+instance Functor (Transform u v) where
+  fmap f (Return x) = Return (f x)
+  fmap f (More cc)  = More (fmap (fmap f) cc)
 
+instance Applicative (Transform u v) where
+  pure = Return
+  cf <*> cx = do
+    f <- cf
+    x <- cx
+    return (f x)
 
-
-
-
-
-
-
-
+instance Monad (Transform u v) where
+  return = Return
+  Return x >>= f = f x
+  More cc  >>= f = More (fmap (>>= f) cc)
 
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = return ()
