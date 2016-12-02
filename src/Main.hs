13f407b50dd4bbbdbb527f75c41b0674546fb3a3
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-02 10:28:38 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 import Prelude hiding (id, (.))
 import Control.Category
+import Control.Monad
+import Data.IORef
+
+batchesOf :: Int -> PTransform u [u]
+batchesOf n = PTransform $ \pullU -> replicateM n pullU
+
+repeatT :: Int -> IO (PTransform u u)
+repeatT n = do
+  ref <- newIORef Nothing
+  return $ PTransform $ \pullU -> do
+    r <- readIORef ref
+    when (fmap fst r == Nothing || fmap fst r == Just 0) $ do
+      u <- pullU
+      writeIORef ref $ Just (n, u)
+    Just (i, u) <- readIORef ref
+    writeIORef ref $ Just (i-1, u)
+    return u
+
+
+
+
+
+
+
 
 newtype PTransform u v = PTransform
   { onPull :: IO u -> IO v
@@ -10,17 +34,6 @@ instance Category PTransform where
   id = PTransform $ \pullU -> pullU
   t2 . t1 = PTransform $ \pullU ->
             onPull t2 (onPull t1 pullU)
-
-
-
-
-
-
-
-
-
-
-
 
 
 main :: IO ()
