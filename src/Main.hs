54b688a4ca2ba918e299866c1fef45fe1b484f47
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-02 13:08:10 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,33 @@
 module Main where
+import Prelude hiding (map)
+
+map :: (u -> v) -> [u] -> [v]
+map _ []     = []
+map f (u:us) = f u : map f us
+
+mapE :: (u -> IO v) -> EList u a -> EList v a
+mapE _       (ReturnE x)             = ReturnE x
+mapE convert (MoreE (EffectE mcc))   = do cc <- effectE mcc
+                                          mapE convert cc
+mapE convert (MoreE (ProduceE u cc)) = do v <- effectE $ convert u
+                                          produceE v
+                                          mapE convert (cc ())
+
+
+addIO :: Int -> Int -> IO Int
+addIO x y = do putStrLn $ "add " ++ show x ++ " and " ++ show y
+               return $ x + y
+
+transform :: EList Int a -> EList Int a
+transform = mapE (addIO 100)
+
+
+
+
 
 
 data EListF u a = EffectE (IO a)
                 | ProduceE u (() -> a)
-
 data EList u a = ReturnE a
                | MoreE (EListF u (EList u a))
 
@@ -15,22 +39,13 @@ produceE :: u -> EList u ()
 produceE u = MoreE (ProduceE u ReturnE)
 
 
-
-
-
-
-
-
-
-
-
 runEList :: EList u a -> (u -> IO ()) -> IO a
 runEList (ReturnE x)    _      = return x
 runEList (MoreE eListF) handle = go eListF
   where
     go (EffectE   mcc) = do cc <- mcc
                             runEList cc handle
-    go (ProduceE v cc) = do handle v
+    go (ProduceE u cc) = do handle u
                             runEList (cc ()) handle
 
 
@@ -41,7 +56,7 @@ runEList (MoreE eListF) handle = go eListF
 
 instance Functor (EListF u) where
   fmap f (EffectE mcc)   = EffectE (fmap f mcc)
-  fmap f (ProduceE v cc) = ProduceE v (fmap f cc)
+  fmap f (ProduceE u cc) = ProduceE u (fmap f cc)
 
 instance Functor (EList u) where
   fmap f (ReturnE x) = ReturnE (f x)
@@ -60,5 +75,21 @@ instance Monad (EList u) where
   MoreE cc  >>= f = MoreE (fmap (>>= f) cc)
 
 
+source :: EList Int ()
+source = mapM_ produceE [0..]
+
+sink :: Show u => EList u a -> IO ()
+sink elist = do
+    putStrLn ""
+    go 10 elist
+  where
+    go :: Show u => Int -> EList u a -> IO ()
+    go 0 _                       = putStrLn "..."
+    go _ (ReturnE _)             = putStrLn "..."
+    go n (MoreE (EffectE mcc))   = do cc <- mcc
+                                      go n cc
+    go n (MoreE (ProduceE u cc)) = do print u
+                                      go (n-1) (cc ())
+
 main :: IO ()
-main = return ()
+main = sink (transform source)
