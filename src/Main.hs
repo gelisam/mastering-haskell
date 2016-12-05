e58ac3e2c649632b949ae356ae939e7f22c2c19f
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-05 11:11:16 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -5,13 +5,13 @@ data Coord = Pos Int Int
 data Size = Size Int Int
 
 data ClickEvent = LeftClick Coord | RightClick Coord
-
+data KeyboardEvent = KeyDown Char | KeyUp Char
 data GUI = Button Label Size | Window [(Coord, GUI)]
 
 
 
 
-wizard :: [ClickEvent] -> [GUI]
+wizard :: [ClickEvent] -> [KeyboardEvent] -> [GUI]
 wizard = undefined
 
 
