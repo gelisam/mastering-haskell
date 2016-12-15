8dd4853a2c1f1623bcdf52320cc3db778c6b8013
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-15 12:21:13 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,28 @@
 module Main where
+import Data.Bool
+
+data Button = Button
+  { buttonGUI    :: Behaviour GUI
+  , buttonPressE :: Event ()
+  }
+
+button :: String -> Rect -> Button
+button label rect = Button
+    { buttonGUI    = bool normalGUI highlightedGUI <$> isHoveringB
+    , buttonPressE = mapFilterE f mouseClickE
+    }
+  where
+    isHoveringB :: Behaviour Bool
+    isHoveringB = isInside <$> mousePositionB <*> pure rect
+    
+    f :: ClickOcc -> Maybe ()
+    f (LeftClick coord) | coord `isInside` rect = Just ()
+    f _                 = Nothing
+
+    normalGUI, highlightedGUI :: GUI
+    (normalGUI, highlightedGUI) = undefined label
 
 
-isHoveringB :: Behaviour Bool
-isHoveringB = (&&) <$> (isInside <$> mousePositionB
-                                 <*> pure buttonRect)
-                   <*> ((== 4) <$> currentPageB)
 
 
 
@@ -21,18 +39,35 @@ isHoveringB = (&&) <$> (isInside <$> mousePositionB
 
 
 
+type Label = String
+data Coord = Pos Int Int
+data Size = Size Int Int
+
+data ClickOcc = LeftClick Coord | RightClick Coord
+data KeyboardOcc = KeyDown Char | KeyUp Char
+data GUI = ButtonGUI Label Size | Window [(Coord, GUI)]
+
+instance Monoid GUI where
+  mempty = Window []
+  mappend g1 g2 = Window (toList g1 ++ toList g2)
+    where
+      toList (Window xs) = xs
+      toList g           = [(Pos 0 0, g)]
 
 
-
-data Coord
 data Rect
 
 isInside :: Coord -> Rect -> Bool
 isInside = undefined
 
-buttonRect :: Rect
-buttonRect = undefined
+rect1 :: Rect
+rect1 = undefined
 
+rect2 :: Rect
+rect2 = undefined
+
+rect3 :: Rect
+rect3 = undefined
 
 
 data Event a
@@ -48,6 +83,9 @@ instance Applicative Behaviour where
   pure  = pureB
   (<*>) = applyB
 
+instance Monoid a => Monoid (Behaviour a) where
+  mempty = pure mempty
+  mappend b1 b2 = mappend <$> b1 <*> b2
 
 neverE :: Event a
 neverE = undefined
@@ -73,14 +111,11 @@ holdB :: a -> Event a -> Behaviour a
 holdB = undefined
 
 
+mouseClickE :: Event ClickOcc
+mouseClickE = undefined
+
 mousePositionB :: Behaviour Coord
 mousePositionB = undefined
-
-nextPageE :: Event ()
-nextPageE = undefined
-
-currentPageB :: Behaviour Int
-currentPageB = undefined
 
 
 main :: IO ()
