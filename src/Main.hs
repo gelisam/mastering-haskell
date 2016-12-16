be5eddcccaea4484525a02c3059c4bb56f956758
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-16 01:21:19 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,20 +2,14 @@ module Main where
 
 data SigTrans a b  -- Signal a -> Signal b
 
---    :: (Signal a -> Signal b)
---    -> (Signal b -> Signal c)
---    -> (Signal a -> Signal c)
-(>>>) :: SigTrans a b -> SigTrans b c -> SigTrans a c
-(>>>) = undefined
 
---  :: (a -> b) -> Signal a -> Signal b
-arr :: (a -> b) -> SigTrans a b
-arr = undefined
 
---    :: (Signal a -> Signal b)
---    -> (Signal (a, c) -> Signal (b, c))
-first :: SigTrans a b -> SigTrans (a, c) (b, c)
-first = undefined
+
+pureS :: a -> Signal a
+pureS = undefined
+
+applyS :: Signal (a -> b) -> Signal a -> Signal b
+applyS = undefined
 
 
 
@@ -25,6 +19,14 @@ first = undefined
 
 
 
+
+
+
+
+
+
+
+data Signal a
 
 
 main :: IO ()
