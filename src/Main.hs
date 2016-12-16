ad584a0e8df7f7ffd941426b3bcf55adcee4c712
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-16 01:21:33 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,15 @@
 module Main where
 
-data SigTrans a b  -- Signal a -> Signal b
 
 
+--                    |
+--       (t0,click),      (t1,click),       ...
+-- False,           True,            False, ...
+toggle1 :: Behaviour Bool
+toggle1 = toggleB (buttonPressE button1)
 
-
-pureS :: b -> SigTrans a b
-pureS = undefined
-
-applyS :: SigTrans a (b -> c) -> SigTrans a b -> SigTrans a c
-applyS = undefined
-
-
-
-
-
+switchBB :: Behaviour (Behaviour a) -> Behaviour a
+switchBB = undefined
 
 
 
@@ -23,6 +18,71 @@ applyS = undefined
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+data Button
+
+buttonPressE :: Button -> Event ()
+buttonPressE = undefined
+
+button1 :: Button
+button1 = undefined
+
+
+
+data Event a
+data Behaviour a
+
+instance Functor Event where
+  fmap _ _ = undefined
+
+instance Functor Behaviour where
+  fmap _ _ = undefined
+
+instance Applicative Behaviour where
+  pure  = pureB
+  (<*>) = applyB
+
+neverE :: Event a
+neverE = undefined
+
+mergeE :: Event a -> Event a -> Event a
+mergeE = undefined
+
+
+pureB :: a -> Behaviour a
+pureB = undefined
+
+applyB :: Behaviour (a -> b) -> Behaviour a -> Behaviour b
+applyB = undefined
+
+applyE :: Behaviour (a -> b) -> Event a -> Event b
+applyE = undefined
+
+
+mapFilterE :: (a -> Maybe b) -> Event a -> Event b
+mapFilterE = undefined
+
+scanE :: (a -> b -> a) -> a -> Event b -> Event a
+scanE = undefined
+
+holdB :: a -> Event a -> Behaviour a
+holdB = undefined
+
+
+toggleB :: Event () -> Behaviour Bool
+toggleB e = holdB False
+          $ scanE (\b () -> not b) False e
 
 main :: IO ()
 main = return ()
