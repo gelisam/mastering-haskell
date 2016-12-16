7fc86755b014aa0cf0c234457d707acfa3ca570a
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-16 10:11:13 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,22 @@
 {-# LANGUAGE RankNTypes #-}
 module Main where
-import Control.Monad.ST
-import Data.STRef
 
+data Reactive a
 
-newIntRef :: ST s (STRef s Int)
-newIntRef = newSTRef 42
+--                    |
+--       (t0,click),      (t1,click),       ...
+-- False,           True,            False, ...
+toggle1 :: Reactive (Behaviour Bool)
+toggle1 = toggleB (buttonPressE button1)
 
-incrIntRef :: STRef s Int -> ST s ()
-incrIntRef ref = modifySTRef ref (+1)
+switchEB' :: Behaviour a
+          -> Event (Reactive (Behaviour a))
+          -> Behaviour a
+switchEB' = undefined
 
-switch :: (forall t. ST t a) -> ST s a
-switch body = return $ runST body
-
-invalid :: ST s Int
-invalid = do
-  ref <- newIntRef
-  _ <- switch $ do
-    ref2 <- newIntRef
-    incrIntRef ref
-    readSTRef ref2
-  readSTRef ref
+switchEB :: Behaviour a
+         -> Event (Behaviour a) -> Behaviour a
+switchEB b ebx = switchEB' b (return <$> ebx)
 
 
 
@@ -29,6 +25,81 @@ invalid = do
 
 
 
+
+
+
+
+
+
+
+
+
+
+data Button
+
+buttonPressE :: Button -> Event ()
+buttonPressE = undefined
+
+button1 :: Button
+button1 = undefined
+
+
+
+instance Functor Reactive where
+  fmap = undefined
+
+instance Applicative Reactive where
+  pure = undefined
+  (<*>) = undefined
+
+instance Monad Reactive where
+  (>>=) = undefined
+
+
+data Event a
+data Behaviour a
+
+instance Functor Event where
+  fmap _ _ = undefined
+
+instance Functor Behaviour where
+  fmap _ _ = undefined
+
+instance Applicative Behaviour where
+  pure  = pureB
+  (<*>) = applyB
+
+neverE :: Event a
+neverE = undefined
+
+mergeE :: Event a -> Event a -> Event a
+mergeE = undefined
+
+
+pureB :: a -> Behaviour a
+pureB = undefined
+
+applyB :: Behaviour (a -> b) -> Behaviour a -> Behaviour b
+applyB = undefined
+
+applyE :: Behaviour (a -> b) -> Event a -> Event b
+applyE = undefined
+
+
+mapFilterE :: (a -> Maybe b) -> Event a -> Event b
+mapFilterE = undefined
+
+scanE :: (a -> b -> a) -> a -> Event b -> Reactive (Event a)
+scanE = undefined
+
+holdB :: a -> Event a -> Reactive (Behaviour a)
+holdB = undefined
+
+
+toggleB :: Event () -> Reactive (Behaviour Bool)
+toggleB e = do
+  e' <- scanE (\b () -> not b) False e
+  holdB False e'
 
 main :: IO ()
 main = return ()
