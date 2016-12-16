46ec15588c2971a40d854b752b217bb1b82fed1e
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-16 12:47:08 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,17 @@
 module Main where
 
-
+data Reactive a
 
 --                    |
 --       (t0,click),      (t1,click),       ...
 -- False,           True,            False, ...
-toggle1 :: Behaviour Bool
+toggle1 :: Reactive (Behaviour Bool)
 toggle1 = toggleB (buttonPressE button1)
 
-switchEB :: Behaviour a
-         -> Event (Behaviour a)
-         -> Behaviour a
-switchEB = undefined
+switchEB' :: Behaviour a
+          -> Event (Reactive (Behaviour a))
+          -> Behaviour a
+switchEB' = undefined
 
 
 
@@ -40,6 +40,17 @@ buttonPressE = undefined
 button1 :: Button
 button1 = undefined
 
+
+
+instance Functor Reactive where
+  fmap = undefined
+
+instance Applicative Reactive where
+  pure = undefined
+  (<*>) = undefined
+
+instance Monad Reactive where
+  (>>=) = undefined
 
 
 data Event a
@@ -75,16 +86,17 @@ applyE = undefined
 mapFilterE :: (a -> Maybe b) -> Event a -> Event b
 mapFilterE = undefined
 
-scanE :: (a -> b -> a) -> a -> Event b -> Event a
+scanE :: (a -> b -> a) -> a -> Event b -> Reactive (Event a)
 scanE = undefined
 
-holdB :: a -> Event a -> Behaviour a
+holdB :: a -> Event a -> Reactive (Behaviour a)
 holdB = undefined
 
 
-toggleB :: Event () -> Behaviour Bool
-toggleB e = holdB False
-          $ scanE (\b () -> not b) False e
+toggleB :: Event () -> Reactive (Behaviour Bool)
+toggleB e = do
+  e' <- scanE (\b () -> not b) False e
+  holdB False e'
 
 main :: IO ()
 main = return ()
