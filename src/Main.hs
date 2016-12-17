91f86e8be632d0899bb95a4ccceda3c87b5cb1cf
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-17 13:44:06 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,21 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
+import Data.List
 
-
-
-type Behaviour a = Int -> a
+data Behaviour a = Cons !a (Behaviour a)
 type Event     a = Behaviour [a]
 
 mergeE :: Event a -> Event a -> Event a
 mergeE e1 e2 = (++) <$> e1 <*> e2
 
+holdB :: a -> Event a -> Behaviour a
+holdB x (Cons xs xss) = let x' = last (x:xs)
+                        in Cons x' (holdB x' xss)
+
+scanE :: (b -> a -> b) -> b -> Event a -> Event b
+scanE f y (Cons xs xss) = let ys = scanl' f y xs
+                              y' = last ys
+                          in Cons (tail ys) (scanE f y' xss)
 
 
 
@@ -23,6 +31,13 @@ mergeE e1 e2 = (++) <$> e1 <*> e2
 
 
 
+
+instance Functor Behaviour where
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+instance Applicative Behaviour where
+  pure x = Cons x (pure x)
+  Cons f fs <*> Cons x xs = Cons (f x) (fs <*> xs)
 
 
 
