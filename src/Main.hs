1efe4b811c87e6f6b5ba7d83a73d850d9faf012a
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-17 12:16:50 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,20 +2,22 @@
 module Main where
 import Data.List
 
-data Behaviour a = Cons !a (Behaviour a)
+data Behaviour a = Branch !a (Behaviour a) (Behaviour a)
 type Event     a = Behaviour [a]
 
 mergeE :: Event a -> Event a -> Event a
 mergeE e1 e2 = (++) <$> e1 <*> e2
 
 holdB :: a -> Event a -> Behaviour a
-holdB x (Cons xs xss) = let x' = last (x:xs)
-                        in Cons x' (holdB x' xss)
-
+holdB x (Branch xs xss1 xss2) = let x' = last (x:xs)
+                                in Branch x' (holdB x' xss1)
+                                             (holdB x' xss2)
 scanE :: (b -> a -> b) -> b -> Event a -> Event b
-scanE f y (Cons xs xss) = let ys = scanl' f y xs
-                              y' = last ys
-                          in Cons (tail ys) (scanE f y' xss)
+scanE f y (Branch xs xss1 xss2) = let ys = scanl' f y xs
+                                      y' = last ys
+                                   in Branch (tail ys)
+                                             (scanE f y' xss1)
+                                             (scanE f y' xss2)
 
 
 
@@ -33,11 +35,15 @@ scanE f y (Cons xs xss) = let ys = scanl' f y xs
 
 
 instance Functor Behaviour where
-  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+  fmap f (Branch x xs1 xs2) = Branch (f x)
+                                     (fmap f xs1)
+                                     (fmap f xs2)
 
 instance Applicative Behaviour where
-  pure x = Cons x (pure x)
-  Cons f fs <*> Cons x xs = Cons (f x) (fs <*> xs)
+  pure x = Branch x (pure x) (pure x)
+  Branch f fs1 fs2 <*> Branch x xs1 xs2 = Branch (f x)
+                                                 (fs1 <*> xs1)
+                                                 (fs2 <*> xs2)
 
 
 
