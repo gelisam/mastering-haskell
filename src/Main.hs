c04b80a32c3d33f6203b7f54c3458e491f135b84
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-17 14:04:29 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
 module Main where
-
+import Data.List
 import Data.Set as Set
 
 type Node = Int
@@ -9,8 +9,20 @@ type Event     a = Node -> [a]
 edges :: Set (Node, Node)
 edges = undefined
 
-mergeE :: Event a -> Event a -> Event a
-mergeE e1 e2 = (++) <$> e1 <*> e2
+predecesors :: Node -> Set Node
+predecesors n = Set.map fst $ Set.filter ((== n) . snd) edges
+
+holdB :: Ord a => a -> Event a -> Behaviour a
+holdB x e n = let xs = Set.map (holdB x e)
+                               (predecesors n)
+                  x' = if Set.null xs then x else undefined
+              in last (x':e n)
+
+scanE :: Ord b => (b -> a -> b) -> b -> Event a -> Event b
+scanE f y e n = let ys = Set.map (holdB y (scanE f y e))
+                                 (predecesors n)
+                    y' = if Set.null ys then y else undefined
+                in tail (scanl' f y' (e n))
 
 
 
