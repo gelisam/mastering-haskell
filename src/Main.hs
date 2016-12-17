f77f1d7763973aa3338f1ed2406238e8eb764f61
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-17 13:46:00 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,12 @@
 module Main where
-
+import Control.Applicative
 
 
 type Behaviour a = Int -> a
 type Event     a = Behaviour (Maybe a)
 
-
-
+mergeE :: Event a -> Event a -> Event a
+mergeE e1 e2 = (<|>) <$> e1 <*> e2
 
 
 
