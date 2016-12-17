c30474939984ffe692c1ec06b12c712d4c49f830
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-16 22:15:28 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,43 @@
 module Main where
+import Control.Concurrent
 import Control.Monad
+import System.Process
+
+type Animation = [Image]
+
+pulsatingCircle :: Animation
+pulsatingCircle = [mkCircle (13.2 * sin t) | t <- [0,0.1..]]
+
+
+
+
+animate :: Animation -> IO ()
+animate []             = return ()
+animate (image:images) = do draw 14 image
+                            threadDelay (50*1000)
+                            _ <- system "clear"
+                            animate images
+
+main :: IO ()
+main = animate pulsatingCircle
+
+
+
+
+
+
+
+
+
+
+
+
 
 data Vec2D = Vec2D Double Double
 type Image = Vec2D -> Char
 
 mkCircle :: Double -> Image
 mkCircle r (Vec2D x y) = if x*x + y*y < r*r then '*' else '.'
-
-zoom3x :: Image -> Image
-zoom3x f (Vec2D x y) = f $ Vec2D (x/3) (y/3)
 
 draw :: Double -> Image -> IO ()
 draw range image = do
@@ -17,6 +46,3 @@ draw range image = do
       putChar $ image $ Vec2D x y
       putChar $ image $ Vec2D x y
     putChar '\n'
-
-main :: IO ()
-main = draw 14 $ zoom3x $ mkCircle 4.4
