92af381cd7ff9a04b3a5357a9be416131cb15c55
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-21 17:00:42 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,23 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
+import Control.Concurrent
+import Control.Monad
+import System.IO.Unsafe
 
-
-
-
-
-
+-- ghc src/Main.hs  -threaded  -rtsopts -with-rtsopts=-N
 main :: IO ()
-main = print $ map fib [0..39]
+main = print $ parMap fib [0..39]
 
 fib :: Int -> Integer
 fib 0 = 1
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
+
+parMap :: (a -> b) -> [a] -> [b]
+parMap f xs = unsafePerformIO $ do
+  vars <- forM xs $ \x -> do
+    var <- newEmptyMVar
+    _ <- forkIO $ do let !y = f x
+                     putMVar var y
+    return var
+  mapM readMVar vars
