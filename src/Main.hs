558695482bb4880a3669b67510a0b51cf42c47a7
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-22 12:28:42 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,31 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, ParallelListComp #-}
 module Main where
+import Control.Concurrent
+import Control.Monad
+
+runPrograms :: [Program a] -> IO [a]
+runPrograms progs = do
+  channels <- replicateM (length progs + 1) newChan
+  outputs <- replicateM (length progs) newEmptyMVar
+  sequence_ [forkIO (runProgram l r o p) | l <- channels
+                                         | r <- tail channels
+                                         | o <- outputs
+                                         | p <- progs
+                                         ]
+  mapM takeMVar outputs
+
+runProgram :: Chan Int -> Chan Int -> MVar a -> Program a -> IO ()
+runProgram _ _ o (Return x)     = putMVar o x
+runProgram l r o (Bind prog cc) = do x <- runProgramF l r prog
+                                     runProgram l r o (cc x)
+
+runProgramF :: Chan Int -> Chan Int -> ProgramF a -> IO a
+runProgramF _ r (Send n) = writeChan r n
+runProgramF l _ Receive  = readChan l
+
+
+
+
 
 data ProgramF a where
   Send    :: Int -> ProgramF ()
@@ -16,13 +42,17 @@ receive :: Program Int
 receive = Bind Receive Return
 
 
-runPrograms :: [Program a] -> IO [a]
-runPrograms = undefined
+instance Functor Program where
+  fmap f (Return x)   = Return (f x)
+  fmap f (Bind px cc) = Bind px (fmap f <$> cc)
 
+instance Applicative Program where
+  pure = Return
+  (<*>) = ap
 
-
-
-
+instance Monad Program where
+  Return x   >>= f = f x
+  Bind px cc >>= f = Bind px ((>>= f) <$> cc)
 
 
 main :: IO ()
