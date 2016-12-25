cf9dd3af3aa847b338e310e8aa4ab198a8cd3e23
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-24 20:11:58 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,61 +1,24 @@
-
-
-
-
-
-
-
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  Manual Parallelism using                   --
-        --  the Free Applicative                       --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        --                                             --
-        -------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+{-# LANGUAGE BangPatterns #-}
+module Main where
+import Control.Concurrent
+import Control.Monad
+import System.IO.Unsafe
 
 
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = print $ parMap fib [0..25]
+
+fib :: Int -> Integer
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+parMap :: (a -> b) -> [a] -> [b]
+parMap f xs = unsafePerformIO $ do
+  vars <- forM xs $ \x -> do
+    var <- newEmptyMVar
+    _ <- forkIO $ do let !y = f x
+                     putMVar var y
+    return var
+  mapM readMVar vars
