a92822c497103ae18b6930ebc575cc42948bc174
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-25 19:36:24 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,24 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, GeneralizedNewtypeDeriving #-}
 module Main where
 import Control.Concurrent
 import Control.Monad
-import System.IO.Unsafe
 
-
+newtype Parallel a = Parallel { runParallel :: IO a }
+  deriving (Functor, Applicative)
 
 main :: IO ()
-main = print $ parMap fib [0..25]
+main = runParallel (parMap (pure . fib) [0..25]) >>= print
 
 fib :: Int -> Integer
 fib 0 = 1
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
-parMap :: (a -> b) -> [a] -> [b]
-parMap f xs = unsafePerformIO $ do
+parMap :: (a -> Parallel b) -> [a] -> Parallel [b]
+parMap f xs = Parallel $ do
   vars <- forM xs $ \x -> do
     var <- newEmptyMVar
-    _ <- forkIO $ do let !y = f x
+    _ <- forkIO $ do !y <- runParallel (f x)
                      putMVar var y
     return var
   mapM readMVar vars
