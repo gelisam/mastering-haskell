d35dc9dfe0815dd61346f35516dd86b9f6ec9a15
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-26 11:31:29 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,38 @@
 {-# LANGUAGE BangPatterns, GeneralizedNewtypeDeriving #-}
 module Main where
-import Control.Concurrent
-import Control.Monad
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+parMap :: (a -> Parallel b) -> [a] -> Parallel [b]
+parMap = undefined
+
+parList :: [Parallel a] -> Parallel [a]
+parList = undefined
+
+parPair :: (Parallel a, Parallel b) -> Parallel (a,b)
+parPair = undefined
+
+
+
+
+
+
+
+
 
 newtype Parallel a = Parallel { runParallel :: IO a }
   deriving (Functor, Applicative)
 
 main :: IO ()
-main = runParallel (parMap (pure . fib) [0..25]) >>= print
-
-fib :: Int -> Integer
-fib 0 = 1
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
-
-parMap :: (a -> Parallel b) -> [a] -> Parallel [b]
-parMap f xs = Parallel $ do
-  vars <- forM xs $ \x -> do
-    var <- newEmptyMVar
-    _ <- forkIO $ do !y <- runParallel (f x)
-                     putMVar var y
-    return var
-  mapM readMVar vars
+main = return ()
