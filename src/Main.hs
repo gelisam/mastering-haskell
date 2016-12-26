17ab9087543ed631b3101e448bb2456491ea186d
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-25 20:40:54 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -17,7 +17,11 @@ instance Applicative Parallel where
 fib :: Int -> Parallel Integer
 fib 0 = pure 1
 fib 1 = pure 1
-fib n = (+) <$> fib (n-1) <*> fib (n-2)
+fib n | n < 20    = Parallel $ (+) <$> runParallel (fib (n-1))
+                                   <*> runParallel (fib (n-2))
+      | otherwise = (+) <$> fib (n-1) <*> fib (n-2)
+
+
 
 main :: IO ()
 main = do
