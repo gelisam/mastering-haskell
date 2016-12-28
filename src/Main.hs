7de3ae9b185b44a360b535a07308743696aad78d
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-27 20:26:51 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE BangPatterns #-}
+module Main where
+import Control.Concurrent
+import Control.Monad
+import System.IO.Unsafe
 
 
 
@@ -5,57 +10,23 @@
 
 
 
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  Laziness and Parallelism                   --
-        --                                             --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        --                                             --
-        -------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+parMap :: (a -> b) -> [a] -> [b]
+parMap f xs = unsafePerformIO $ do
+  vars <- forM xs $ \x -> do
+    var <- newEmptyMVar
+    _ <- forkIO $ do let !y = f x
+                     putMVar var y
+    return var
+  mapM readMVar vars
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = do
+  let r = parMap fib [10,20]
+  print r
+
+
+
+fib :: Int -> Integer
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
