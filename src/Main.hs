c59e7822ea6b373cb147a87324655241892a995f
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-31 12:48:34 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,61 +1,27 @@
+{-# LANGUAGE RecordWildCards #-}
+module Main where
+import Control.Monad
+import Data.IORef
 
+data Counter = Counter { field1 :: IORef Int
+                       , field2 :: IORef Int
+                       }
 
+newCounter :: IO Counter
+newCounter = Counter <$> newIORef 0 <*> newIORef 0
 
+assertInvariant :: Counter -> IO ()
+assertInvariant (Counter {..}) = do r <- (==) <$> readIORef field1
+                                              <*> readIORef field2
+                                    when (not r) $ fail "violation"
 
-
-
-
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  Purity and Parallelism                     --
-        --                                             --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        --                                             --
-        -------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+increment :: Counter -> IO ()
+increment c@(Counter {..}) = do assertInvariant c
+                                modifyIORef' field1 (+1)
+                                modifyIORef' field2 (+1)
+                                assertInvariant c
 
 
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = return ()
