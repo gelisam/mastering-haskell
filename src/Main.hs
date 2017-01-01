1d356403c18ceabe5365096128e3baa232cef6e3
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-01 02:34:00 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,33 @@
 {-# LANGUAGE RecordWildCards #-}
 module Main where
+import Control.Concurrent
 import Control.Monad
 import Data.IORef
+import System.IO
 
-data Counter = Counter { field1 :: IORef Int
-                       , field2 :: IORef Int
-                       }
+main :: IO ()
+main = do
+  counter <- newCounter
+  forever $ do increment counter
+               printCounter counter
+
+printCounter :: Counter -> IO ()
+printCounter (Counter {..}) = do
+  n <- readIORef field1
+  putStr $ show n ++ ", "
+  hFlush stdout
+  threadDelay (200 * 1000)
+
+
+
+
+
+
+
+data Counter = Counter
+  { field1 :: IORef Int
+  , field2 :: IORef Int
+  }
 
 newCounter :: IO Counter
 newCounter = Counter <$> newIORef 0 <*> newIORef 0
@@ -20,8 +42,3 @@ increment c@(Counter {..}) = do assertInvariant c
                                 modifyIORef' field1 (+1)
                                 modifyIORef' field2 (+1)
                                 assertInvariant c
-
-
-
-main :: IO ()
-main = return ()
