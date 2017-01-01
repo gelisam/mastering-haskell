58c1f459eb53bef6b883d5b83579c3db15a42e7f
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-01 13:49:01 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,19 @@
 {-# LANGUAGE BangPatterns #-}
 module Main where
 import Control.Concurrent
+import System.IO
+import System.IO.Unsafe
 
-
-
-
-
-
-
-
-
-type Counter = MVar Int
-
-newCounter :: IO Counter
-newCounter = newMVar 0
+main :: IO ()
+main = do
+  counter <- newCounter
+  let sharedThunk :: Int
+      sharedThunk = unsafePerformIO $ do
+        increment counter
+        return 42
+  let !_ = sharedThunk + sharedThunk
+  printCounter counter
+  main
 
 increment :: Counter -> IO ()
 increment counter = do
@@ -30,6 +30,36 @@ increment counter = do
 
 
 
+type Counter = MVar Int
 
-main :: IO ()
-main = return ()
+newCounter :: IO Counter
+newCounter = newMVar 0
+
+printCounter :: Counter -> IO ()
+printCounter counter = do
+  n <- takeMVar counter
+  putStr $ show n ++ ", "
+  hFlush stdout
+  threadDelay (200 * 1000)
+
+
+
+newtype Parallel a = Parallel { runParallel :: IO a }
+
+instance Functor Parallel where
+  fmap f (Parallel ioX) = Parallel $ do !x <- ioX
+                                        return (f x)
+
+instance Applicative Parallel where
+  pure = Parallel . pure
+  Parallel ioF <*> Parallel ioX = Parallel $ do
+    varF <- newEmptyMVar
+    varX <- newEmptyMVar
+    _ <- forkIO $ do !f <- ioF
+                     putMVar varF f
+    _ <- forkIO $ do !x <- ioX
+                     putMVar varX x
+    takeMVar varF <*> takeMVar varX
+
+parPair :: (a, b) -> Parallel (a, b)
+parPair (x, y) = (,) <$> pure x <*> pure y
