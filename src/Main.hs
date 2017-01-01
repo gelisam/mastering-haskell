ea6c71d2cd9ef5ab53114e07d3f65b98323a86e4
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-31 22:53:46 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -11,17 +11,18 @@ main = do
   forever $ do runThreads $ replicate 2 $ increment counter
                printCounter counter
 
-runThreads :: [IO a] -> IO ()
-runThreads threads = do
-  vars <- forM threads $ \thread -> do
-    var <- newEmptyMVar
-    void $ forkFinally (do {!_ <- thread; putMVar var (Right ())})
-                       (putMVar var)
-    return var
-  rs <- mapM takeMVar vars
-  forM_ rs $ \r -> case r of
-    Left e -> error (show e)
-    Right _ -> return ()
+data Counter = Counter { mutex  :: MVar ()
+                       , field1 :: IORef Int
+                       , field2 :: IORef Int
+                       }
+
+increment :: Counter -> IO ()
+increment c@(Counter {..}) = do takeMVar mutex
+                                assertInvariant c
+                                modifyIORef' field1 (+1)
+                                modifyIORef' field2 (+1)
+                                assertInvariant c
+                                putMVar mutex ()
 
 
 
@@ -39,23 +40,23 @@ printCounter (Counter {..}) = do
   hFlush stdout
   threadDelay (200 * 1000)
 
+runThreads :: [IO a] -> IO ()
+runThreads threads = do
+  vars <- forM threads $ \thread -> do
+    var <- newEmptyMVar
+    void $ forkFinally (do {!_ <- thread; putMVar var (Right ())})
+                       (putMVar var)
+    return var
+  rs <- mapM takeMVar vars
+  forM_ rs $ \r -> case r of
+    Left e -> error (show e)
+    Right _ -> return ()
 
-
-data Counter = Counter
-  { field1 :: IORef Int
-  , field2 :: IORef Int
-  }
 
 newCounter :: IO Counter
-newCounter = Counter <$> newIORef 0 <*> newIORef 0
+newCounter = Counter <$> newMVar () <*> newIORef 0 <*> newIORef 0
 
 assertInvariant :: Counter -> IO ()
 assertInvariant (Counter {..}) = do r <- (==) <$> readIORef field1
                                               <*> readIORef field2
                                     when (not r) $ fail "violation"
-
-increment :: Counter -> IO ()
-increment c@(Counter {..}) = do assertInvariant c
-                                modifyIORef' field1 (+1)
-                                modifyIORef' field2 (+1)
-                                assertInvariant c
