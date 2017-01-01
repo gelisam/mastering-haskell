30873597807b84eb06fc988f794b9aee9dbf8072
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-01 13:45:10 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,72 +1,35 @@
 {-# LANGUAGE BangPatterns #-}
 module Main where
 import Control.Concurrent
-import System.IO
+
+
+
+
+
+
+
+
+
+type Counter = MVar Int
+
+newCounter :: IO Counter
+newCounter = newMVar 0
+
+increment :: Counter -> IO ()
+increment counter = do
+  n <- takeMVar counter
+  let !n' = n + 1
+  putMVar counter n'
+
+
+
+
+
+
+
+
+
+
 
 main :: IO ()
-main = go newCounter
-  where
-    go :: Counter -> IO ()
-    go c = do let pair = (increment c, increment c)
-              (c1,c2) <- runParallel $ parPair pair
-              let c' = mergeDeltas c c1 c2
-              printCounter c'
-              go c'
-
-mergeDeltas :: Counter -> Counter -> Counter -> Counter
-mergeDeltas (Counter m0 n0)
-            (Counter m1 n1)
-            (Counter m2 n2) = Counter (m1+m2-m0) (n1+n2-n0)
-
-
-
-
-
-
-
-
-
-
-
-data Counter = Counter { field1 :: !Int
-                       , field2 :: !Int
-                       }
-
-newCounter :: Counter
-newCounter = Counter 0 0
-
-assertInvariant :: Counter -> Counter
-assertInvariant c@(Counter m n) | m == n    = c
-                                | otherwise = error "violation"
-
-increment :: Counter -> Counter
-increment = assertInvariant
-          . (\(Counter m n) -> Counter (m+1) (n+1))
-          . assertInvariant
-
-printCounter :: Counter -> IO ()
-printCounter (Counter _ n) = do
-  putStr $ show n ++ ", "
-  hFlush stdout
-  threadDelay (200 * 1000)
-
-
-newtype Parallel a = Parallel { runParallel :: IO a }
-
-instance Functor Parallel where
-  fmap f (Parallel ioX) = Parallel $ do !x <- ioX
-                                        return (f x)
-
-instance Applicative Parallel where
-  pure = Parallel . pure
-  Parallel ioF <*> Parallel ioX = Parallel $ do
-    varF <- newEmptyMVar
-    varX <- newEmptyMVar
-    _ <- forkIO $ do !f <- ioF
-                     putMVar varF f
-    _ <- forkIO $ do !x <- ioX
-                     putMVar varX x
-    takeMVar varF <*> takeMVar varX
-
-parPair :: (a, b) -> Parallel (a, b)
-parPair (x, y) = (,) <$> pure x <*> pure y
+main = return ()
