b594d011e7d77b6222f8729e15e98a90d2fb0092
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-01 15:52:04 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,57 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
+import Control.Concurrent
 import System.IO.Unsafe
 
-x, y :: Int
-x = unsafePerformIO $ return y
-y = unsafePerformIO $ return x
+noisyPlus :: Num a => a -> a -> a
+noisyPlus x y = traceThread "cache" $ x + y
+
+cache :: [Integer]
+cache = take 10 $ 1:1:zipWith noisyPlus cache (tail cache)
+
+fib :: Int -> Integer
+fib n | n <  10   = cache !! n
+      | otherwise = traceThread "fib" $ fib (n-1) + fib (n-2)
 
 main :: IO ()
-main = print x
+main = traceThread "main" $ do
+  r <- runParallel $ parPair (fib 11, fib 12)
+  print r
+
+
+
+
+
+
+
+
+
+traceThread :: String -> a -> a
+traceThread msg x = unsafePerformIO $ do
+  threadId <- myThreadId
+  putStrLn $ "[" ++ show threadId ++ "] " ++ msg
+  return x
+
+
+
+newtype Parallel a = Parallel { runParallel :: IO a }
+
+instance Functor Parallel where
+  fmap f (Parallel ioX) = Parallel $ do !x <- ioX
+                                        return (f x)
+
+instance Applicative Parallel where
+  pure = Parallel . pure
+  Parallel ioF <*> Parallel ioX = Parallel $ do
+    varF <- newEmptyMVar
+    varX <- newEmptyMVar
+    _ <- forkIO $ traceThread "thread"
+                $ do !f <- ioF
+                     putMVar varF f
+    _ <- forkIO $ traceThread "thread"
+                $ do !x <- ioX
+                     putMVar varX x
+    takeMVar varF <*> takeMVar varX
+
+parPair :: (a, b) -> Parallel (a, b)
+parPair (x, y) = (,) <$> pure x <*> pure y
