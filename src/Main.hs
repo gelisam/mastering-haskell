aa599853415ac9ccd91a989352880d74dcc97cd2
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-03 13:32:16 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,15 @@
 module Main where
-import Control.Concurrent
-import Control.Monad
 
-runInParallel :: IO a -> IO b -> IO (a,b)
-runInParallel ioX ioY = do
-  varX <- newIVar
-  varY <- newIVar
-  _ <- forkIO $ putIVar varX =<< ioX
-  _ <- forkIO $ putIVar varY =<< ioY
-  (,) <$> readIVar varX <*> readIVar varY
-
-
-type IVar a = MVar a
-
-newIVar :: IO (IVar a)
-newIVar = newEmptyMVar
-
-readIVar :: IVar a -> IO a
-readIVar var = readMVar var
-
-putIVar :: IVar a -> a -> IO ()
-putIVar var x = do r <- tryPutMVar var x
-                   when (not r) $ fail "double put"
+-- do before             do before
+--                          
+--    ...                   ...
+--    
+--    var1 <- newIVar
+--                          var2 <- newIVar
+--    
+--    ...                   ...
+--    
+--    after                 after
 
 
 
