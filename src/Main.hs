d6378ffdf32d0f8b664a9ba9ceabd843917fe595
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-03 19:40:26 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,52 +1,59 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BangPatterns #-}
 module Main where
 import Control.Concurrent
 import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Trans.State
-
-raceM :: MVar a -> MVar a -> IO a
-raceM var1 var2 = tryReadMVar var1 >>= \case
-  Just x  -> return x
-  Nothing -> tryReadMVar var2 >>= \case
-    Just x  -> return x
-    Nothing -> raceM var1 var2
-
-raceI :: IVar (Progress a) -> IVar (Progress a) -> IO a
-raceI var1 var2 = runStateT tryReadIVar var1 >>= \case
-  (Just x, _)      -> return x
-  (Nothing, var1') -> runStateT tryReadIVar var2 >>= \case
-    (Just x, _)      -> return x
-    (Nothing, var2') -> raceI var1' var2'
+import System.IO.Unsafe
 
 
 
+cache :: [Integer]
+cache =       take 10 $ 1:1:zipWith noisyPlus cache (tail cache)
 
 
+
+fib :: Int -> IO Integer
+fib     n | n <  10   = return (cache !! n)
+          | otherwise = traceThread "fib"
+                      $ (+) <$> fib     (n-1) <*> fib     (n-2)
+
+main :: IO ()
+main = traceThread "main" $ do
+  
+  
+  runThreads $ [fib     11, fib     12]
 
 
 
 
 
 
-type Step a = IVar (Progress a) -> IO ()
-data Progress a = Done a | More (Step a)
 
-yield :: Step a -> Step a
-yield cc var = putIVar var (More cc)
 
-done :: a -> Step a
-done s var = putIVar var (Done s)
 
-tryReadIVar :: StateT (IVar (Progress a)) IO (Maybe a)
-tryReadIVar = do var <- get
-                 r <- liftIO $ readIVar var
-                 case r of Done x  -> return (Just x)
-                           More cc -> do
-                             var' <- liftIO newIVar
-                             void $ liftIO $ forkIO $ cc var'
-                             put var'
-                             return Nothing
+traceThread :: String -> a -> a
+traceThread msg x = unsafePerformIO $ do
+  threadId <- myThreadId
+  putStrLn $ "[" ++ show threadId ++ "] " ++ msg
+  return x
+
+noisyPlus :: Num a => a -> a -> a
+noisyPlus x y = traceThread "cache" $ x + y
+
+runThreads :: [IO a] -> IO ()
+runThreads threads = do
+  -- spawn a dummy thread so that the thread numbers which
+  -- compute fib are the same in this slide and the next
+  void $ forkIO $ return ()
+  
+  vars <- forM threads $ \thread -> do
+    var <- newEmptyMVar
+    void $ forkFinally (do {!_ <- thread; putMVar var (Right ())})
+                       (putMVar var)
+    return var
+  rs <- mapM takeMVar vars
+  forM_ rs $ \r -> case r of
+    Left e -> error (show e)
+    Right _ -> return ()
 
 
 
@@ -61,8 +68,3 @@ readIVar var = readMVar var
 putIVar :: IVar a -> a -> IO ()
 putIVar var x = do r <- tryPutMVar var x
                    when (not r) $ fail "double put"
-
-
-
-main :: IO ()
-main = return ()
