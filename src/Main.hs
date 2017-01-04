f712d441ea339bb959dee580e8199c71114bf985
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-03 19:24:06 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,50 @@
 module Main where
+import Control.Concurrent
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.State
 
--- do before             do before
---                          
---    ...                   ...
---    
---                          x <- readIVar var
---                          BLOCKS
---    writeIVar var x
---    ...                   ...
---    
---    after                 after
+data MsgQueue s   = Cons s (IVar (MsgQueue s))
+type QueueT s m a = StateT (IVar (MsgQueue s)) m a
+
+sendMessage :: MonadIO m => s -> QueueT s m ()
+sendMessage s = do var <- get
+                   var' <- liftIO newIVar
+                   liftIO $ putIVar var $ Cons s var'
+
+nextMessage :: MonadIO m => QueueT s m s
+nextMessage = do var <- get
+                 Cons x var' <- liftIO $ readIVar var
+                 put var'
+                 return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+type IVar a = MVar a
+
+newIVar :: IO (IVar a)
+newIVar = newEmptyMVar
+
+readIVar :: IVar a -> IO a
+readIVar var = readMVar var
+
+putIVar :: IVar a -> a -> IO ()
+putIVar var x = do r <- tryPutMVar var x
+                   when (not r) $ fail "double put"
 
 
 
