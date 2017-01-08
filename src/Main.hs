25153187f6f895313b83b11305e67d3834e867d5
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-08 17:00:45 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,26 @@
 module Main where
+import Data.MultiSet as MultiSet
 
+data CmdI a = ReadI      | WriteI a  deriving (Eq, Ord)
+data CmdF   = IncrementF | FreezeF   deriving (Eq, Ord)
+data CmdE   = IncrementE | ReadEvenE deriving (Eq, Ord)
 
-data CmdI a = ReadI      | WriteI a
-data CmdF   = IncrementF | FreezeF
-data CmdE   = IncrementE | ReadEvenE
+class LVish c where
+  isAllowed :: c -> MultiSet c -> Bool
 
-class LVish c
-instance LVish (CmdI a)
-instance LVish CmdF
---instance LVish CmdE
+instance Ord a => LVish (CmdI a) where
+  isAllowed ReadI      = const True
+  isAllowed (WriteI x) = MultiSet.null . MultiSet.filter go
+    where go (WriteI x') = x' /= x
+          go _           = False
 
+instance LVish CmdF where
+  isAllowed FreezeF    = const True
+  isAllowed IncrementF = MultiSet.notMember FreezeF
+
+instance LVish CmdE where
+  isAllowed IncrementE = const True
+  isAllowed ReadEvenE  = even . MultiSet.occur IncrementE
 
 
 
