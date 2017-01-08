9f36438b9f3c645136b00abee0b8c3014d804e2b
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-07 20:28:59 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,35 @@
 module Main where
+import Control.Concurrent
 import Control.Monad
 
-
-fib :: Int -> Integer
-fib 0 = 1
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+fib :: MVar Integer -> Int -> IO ()
+fib var 0 = modifyMVar var $ \x -> return (x+1, ())
+fib var 1 = modifyMVar var $ \x -> return (x+1, ())
+fib var n = fib var (n-1) >> fib var (n-2)
 
 main :: IO ()
 main = forM_ [10,20,30,40] $ \n -> do
-  print (fib n > 100)
+  print =<< isFibLarge n
+
+isFibLarge :: Int -> IO Bool
+isFibLarge n = do
+  var <- newMVar 0
+  raceM (True <$ waitUntil (> 100) var)
+        (do fib var n
+            (> 100) <$> readMVar var)
+
+waitUntil :: (a -> Bool) -> MVar a -> IO ()
+waitUntil p var = do x <- readMVar var
+                     unless (p x) $ do yield
+                                       waitUntil p var
+
+
+
+
+
+raceM :: IO a -> IO a -> IO a
+raceM ioX1 ioX2 = do
+  var <- newEmptyMVar
+  _ <- forkIO $ do x1 <- ioX1; x1 `seq` putMVar var x1
+  _ <- forkIO $ do x2 <- ioX2; x2 `seq` putMVar var x2
+  readMVar var
