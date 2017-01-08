5118681b15f610db3d98d5d92a4e05c659a5e681
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-07 20:23:51 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,20 @@
-{-# LANGUAGE LambdaCase #-}
 module Main where
-import Control.Concurrent
+
+-- do before             do before
+--                          
+--    ...                   ...
+--    
+--    writeLVar var x1
+--                          writeLVar var x2
+--    
+--    ...                   ERROR
+--    
+--    after
 
 
-data LState a = Empty | Full a
-type LVar a = MVar (LState a)
 
-newLVar :: IO (LVar a)
-newLVar = newMVar Empty
 
-readLVar :: LVar a -> IO a
-readLVar var = readMVar var >>= \case
-                 Full x -> return x
-                 Empty  -> do yield
-                              readLVar var
 
-putLVar :: Eq a => LVar a -> a -> IO ()
-putLVar var x = takeMVar var >>= \case
-                  Full x' | x == x' -> putMVar var (Full x)
-                  Empty             -> putMVar var (Full x)
-                  _                 -> fail "incompatible put"
 
 
 
