4bde66cd9f5ae0d0a5fd29daa5a36cf2740cc1b7
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-09 16:43:51 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,32 @@
 module Main where
+import Prelude hiding ((||))
 
-fib :: Int -> Integer
-fib 0 = 1
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+(||) :: Bool -> Bool -> Bool
+True  || _ = True
+False || b = b
 
 main :: IO ()
 main = do
   putStrLn "go!"
   print $ (fib 30 > 100) || (fib 40 > 100)
   print $ (fib 40 > 100) || (fib 30 > 100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fib :: Int -> Integer
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
