db1192d4d60aa02fe5730c51c4c5e4ce22d6025f
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-09 10:03:58 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -3,13 +3,16 @@ module Main where
 -- do ...                   ...
 --    
 --                          apply c''' var
+--                          apply c''  var
 --    _ <- apply c var
 --    ERROR
---                          apply c''  var
 --                          apply c'   var
 --    
 --                          ...
 
+stubborn :: Property
+stubborn = property $ \c cs cs' -> isDisallowed c cs
+                         `implies` isDisallowed c (cs `union` cs')
 
 
 
@@ -23,8 +26,21 @@ module Main where
 
 
 
+data Property
+data Cmd a
+data Cmds
 
+property :: (Cmd Int -> Cmds -> Cmds -> Bool) -> Property
+property = undefined
 
+implies :: Bool -> Bool -> Bool
+implies = undefined
+
+union :: Cmds -> Cmds -> Cmds
+union = undefined
+
+isDisallowed :: Cmd Int -> Cmds -> Bool
+isDisallowed = undefined
 
 main :: IO ()
 main = return ()
