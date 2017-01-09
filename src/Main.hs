e5de8c1fb51939fe2eb185b2461102b06c0900a4
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-09 10:04:13 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -5,14 +5,20 @@ module Main where
 --                          apply c''' var
 --                          apply c''  var
 --                          apply c'   var
---    _ <- apply c var
---    ERROR
+--    x1 <- apply c var
 --    
---                          ...
+--    ...                   ...
 
 stubborn :: Property
-stubborn = property $ \c cs cs' -> isDisallowed c cs
-                         `implies` isDisallowed c (cs `union` cs')
+stubborn = property $ \c cs (SomeR c')
+        -> ( result c cs /= Nothing
+          && isAllowed c' cs
+          && isAllowed c (insert (SomeR c') cs)
+          && isAllowed c' (insert (SomeR c) cs)
+           ) `implies` (
+             result c (insert (SomeR c') cs)
+          == result c cs
+           )
 
 
 
@@ -29,18 +35,26 @@ stubborn = property $ \c cs cs' -> isDisallowed c cs
 data Property
 data Cmd a
 data Cmds
+data Result
+data SomeR = SomeR (Cmd Int)
 
-property :: (Cmd Int -> Cmds -> Cmds -> Bool) -> Property
+property :: (Cmd Int -> Cmds -> SomeR -> Bool) -> Property
 property = undefined
 
 implies :: Bool -> Bool -> Bool
 implies = undefined
 
-union :: Cmds -> Cmds -> Cmds
-union = undefined
+result :: Cmd Int -> Cmds -> Maybe Int
+result = undefined
+
+isAllowed :: Cmd Int -> Cmds -> Bool
+isAllowed = undefined
 
 isDisallowed :: Cmd Int -> Cmds -> Bool
 isDisallowed = undefined
+
+insert :: SomeR -> Cmds -> Cmds
+insert = undefined
 
 main :: IO ()
 main = return ()
