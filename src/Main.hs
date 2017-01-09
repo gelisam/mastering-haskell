26d76a4a4f2bceb8927fe9cea45c2fdc131a1ac3
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-09 17:49:18 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -5,10 +5,10 @@ data CmdI a = ReadI      | WriteI a
 data CmdF   = IncrementF | FreezeF
 data CmdE   = IncrementE | ReadEvenE
 
-
-
-
-
+class LVish c
+instance LVish (CmdI a)
+instance LVish CmdF
+--instance LVish CmdE
 
 
 
