1c6f426c0fd747a09f757d13816e1d6b5ef53b91
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-08 20:37:54 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances, GADTs #-}
 module Main where
 import Data.MultiSet as MultiSet
 
@@ -7,8 +7,18 @@ data CmdE r where
   ReadE      :: CmdE Int
 
 class LVish c where
-  isAllowed :: c r -> MultiSet c -> Bool
-  result    :: c r -> MultiSet c -> Maybe r
+  isAllowed :: c r -> MultiSet (SomeR c) -> Bool
+  result    :: c r -> MultiSet (SomeR c) -> Maybe r
+
+data SomeR c where
+  SomeR :: c r -> SomeR c
+
+
+instance LVish CmdE where
+  isAllowed IncrementE = const True
+  isAllowed ReadE      = const True
+  result IncrementE = const $ return ()
+  result ReadE      = return . MultiSet.occur (SomeR IncrementE)
 
 
 
@@ -22,9 +32,16 @@ class LVish c where
 
 
 
+instance Eq (SomeR CmdE) where
+  SomeR IncrementE == SomeR IncrementE = True
+  SomeR ReadE      == SomeR ReadE      = True
+  _                == _                = False
 
-
-
+instance Ord (SomeR CmdE) where
+  SomeR IncrementE <= SomeR IncrementE = True
+  SomeR IncrementE <= SomeR ReadE      = True
+  SomeR ReadE      <= SomeR ReadE      = True
+  _                <= _                = False
 
 
 
