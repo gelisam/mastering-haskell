b7de26d7a8a8bb4f05438816c73ca1797771ab93
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-09 10:13:12 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,9 @@
+{-# LANGUAGE GADTs #-}
 module Main where
 
--- do ...                   ...
---    
---    x1 <- apply c var
---                          apply c''' var
---                          apply c''  var
---                          apply c'   var
---    
---    ...                   ...
-
-stubborn :: Property
-stubborn = property $ \c cs (SomeR c')
-        -> ( result c cs /= Nothing
-          && isAllowed c' cs
-          && isAllowed c (insert (SomeR c') cs)
-          && isAllowed c' (insert (SomeR c) cs)
-           ) `implies` (
-             result c (insert (SomeR c') cs)
-          == result c cs
-           )
+data CmdI a r where
+  ReadI ::      CmdI a a
+  Write :: a -> CmdI a ()
 
 
 
@@ -32,29 +17,11 @@ stubborn = property $ \c cs (SomeR c')
 
 
 
-data Property
-data Cmd a
-data Cmds
-data Result
-data SomeR = SomeR (Cmd Int)
 
-property :: (Cmd Int -> Cmds -> SomeR -> Bool) -> Property
-property = undefined
 
-implies :: Bool -> Bool -> Bool
-implies = undefined
 
-result :: Cmd Int -> Cmds -> Maybe Int
-result = undefined
 
-isAllowed :: Cmd Int -> Cmds -> Bool
-isAllowed = undefined
 
-isDisallowed :: Cmd Int -> Cmds -> Bool
-isDisallowed = undefined
-
-insert :: SomeR -> Cmds -> Cmds
-insert = undefined
 
 main :: IO ()
 main = return ()
