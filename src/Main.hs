6533745545254cd9a69f84cf3ae203c3cbb5851e
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-09 18:45:01 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,12 @@
-{-# LANGUAGE FlexibleContexts, GADTs #-}
 module Main where
-import Control.Concurrent
-import Data.MultiSet as MultiSet
 
-type LVar c = MVar (MultiSet (SomeR c))
-
-newLVar :: IO (LVar a)
-newLVar = newMVar MultiSet.empty
-
-apply :: (LVish c, Ord (SomeR c)) => c a -> LVar c -> IO a
-apply c var = do
-  cs <- takeMVar var
-  if isAllowed c cs
-  then case result c cs of
-         Just x  -> do putMVar var $ MultiSet.insert (SomeR c) cs
-                       return x
-         Nothing -> do putMVar var cs
-                       yield
-                       apply c var
-  else fail "command not allowed"
+-- do ...                   ...
+--    
+--    x <- apply c var
+--    
+--                          apply c' var
+--    
+--    ...                   ...
 
 
 
@@ -31,18 +19,7 @@ apply c var = do
 
 
 
-implies :: Bool -> Bool -> Bool
-implies x y = y || not x
 
-
-
-
-class LVish c where
-  isAllowed :: c r -> MultiSet (SomeR c) -> Bool
-  result    :: c r -> MultiSet (SomeR c) -> Maybe r
-
-data SomeR c where
-  SomeR :: c r -> SomeR c
 
 
 
