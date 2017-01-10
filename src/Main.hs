4f99f0f9d658c2578c86a62165c3118e78944ae3
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-10 10:31:41 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,61 +1,23 @@
+module Main where
+import Control.Concurrent
+import Control.Monad
 
-
-
-
-
-
-
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  Manual Concurrency using forkIO            --
-        --                                             --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        --                                             --
-        -------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- like threadDelay, but using seconds instead of microseconds
+sleep :: Double -> IO ()
+sleep seconds = threadDelay $ round $ seconds * 1000 * 1000
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = do
+  
+  _ <- forkIO $ do
+    replicateM_ 4 $ do
+      sleep 0.5
+      putStrLn "thread"
+  
+  
+  sleep 0.25
+  
+  
+  replicateM_ 4 $ do
+    sleep 0.5
+    putStrLn "main"
