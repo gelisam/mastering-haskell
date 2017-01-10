3895f7368f20ee0d710b86d7dab87c202b5fb78a
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-10 08:23:47 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,35 +1,49 @@
 module Main where
-import Control.Concurrent
-import Control.Monad
+import Numeric.Natural
 
-fib :: MVar Integer -> Int -> IO ()
-fib var 0 = modifyMVar var $ \x -> return (x+1, ())
-fib var 1 = modifyMVar var $ \x -> return (x+1, ())
-fib var n = fib var (n-1) >> fib var (n-2)
+-- IVar with double put
+data CmdI a = Read   -- block until Put
+            | Put a  -- conflicts with (Put x') if x /= x'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 main :: IO ()
-main = forM_ [10,20,30,40] $ \n -> do
-  print =<< isFibLarge n
-
-isFibLarge :: Int -> IO Bool
-isFibLarge n = do
-  var <- newMVar 0
-  raceM (True <$ waitUntil (> 100) var)
-        (do fib var n
-            (> 100) <$> readMVar var)
-
-waitUntil :: (a -> Bool) -> MVar a -> IO ()
-waitUntil p var = do x <- readMVar var
-                     unless (p x) $ do yield
-                                       waitUntil p var
-
-
-
-
-
-raceM :: IO a -> IO a -> IO a
-raceM ioX1 ioX2 = do
-  var <- newEmptyMVar
-  _ <- forkIO $ do x1 <- ioX1; x1 `seq` putMVar var x1
-  _ <- forkIO $ do x2 <- ioX2; x2 `seq` putMVar var x2
-  readMVar var
+main = return ()
