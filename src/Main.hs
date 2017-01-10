8a85c5cbe1dc920b025afc1e0559323e0fbb92b6
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-10 09:34:57 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -5,13 +5,13 @@ import Numeric.Natural
 data CmdI a = Read   -- block until Put
             | Put a  -- conflicts with (Put x') if x /= x'
 
-
-
-
-
-
-
-
+-- (Bool, Bool)
+data CmdB = PutFst Bool  -- conflicts with (PutFst b') if b /= b'
+          | PutSnd Bool  -- conflicts with (PutSnd b') if b /= b'
+          | ReadFst      -- block until (PutFst b)
+          | ReadSnd      -- block until (PutSnd b)
+          | ReadAnd      -- block until one False or two Trues
+          | ReadOr       -- block until one True or two Falses
 
 
 
