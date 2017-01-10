fdd4f043da3f63df1ba944e1a43b19e289b0af57
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-10 07:56:34 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,44 +1,5 @@
-{-# LANGUAGE BangPatterns #-}
 module Main where
-import Control.Concurrent
-
-parOr :: Bool -> Bool -> IO Bool
-parOr b1 b2 = raceI (return $ b1 || b2)
-                    (return $ b2 || b1)
-
-raceI :: IO a -> IO a -> IO a
-raceI ioX1 ioX2 = do
-  var <- newIVar
-  _ <- forkIO $ do !x1 <- ioX1; putIVar var x1
-  _ <- forkIO $ do !x2 <- ioX2; putIVar var x2
-  readIVar var
-
-main :: IO ()
-main = do
-  putStrLn "go!"
-  print =<< parOr (fib 30 > 100) (fib 40 > 100)
-  print =<< parOr (fib 40 > 100) (fib 30 > 100)
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  let loop = do threadDelay (1000 * 1000)
-                loop
-  loop
-
-
-
-
-
-
-
+import Control.Monad
 
 
 fib :: Int -> Integer
@@ -46,17 +7,6 @@ fib 0 = 1
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
-
-
-type IVar a = MVar a
-
-newIVar :: IO (IVar a)
-newIVar = newEmptyMVar
-
-readIVar :: IVar a -> IO a
-readIVar var = readMVar var
-
-putIVar :: IVar a -> a -> IO ()
-putIVar var x = do r <- tryPutMVar var x
-                   if r then return ()
-                        else fail "double put"
+main :: IO ()
+main = forM_ [10,20,30,40] $ \n -> do
+  print (fib n > 100)
