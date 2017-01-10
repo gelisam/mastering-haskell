ea48f02dbc0e6dc3e9cd94221905931ff03c06f4
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-10 15:06:54 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,45 @@ module Main where
 import Control.Concurrent
 import Control.Monad
 
-main :: IO ()
-main = do
-  putStrLn "2 + 2 = ?"; syncAdd 2 2 >>= print
-  putStrLn "3 + 3 = ?"; syncAdd 3 3 >>= print
-  putStrLn "4 + 4 = ?"; syncAdd 4 4 >>= print
-  forever $ sleep 1
+
+data Command = AsyncAdd Int Int (Int -> Program ())
+             | PrintStr String
+             | PrintInt Int
+
+runCommand :: Command -> IO ()
+runCommand (AsyncAdd x1 x2 cc) = asyncAdd x1 x2 (runProgram . cc)
+runCommand (PrintStr s)        = putStrLn s
+runCommand (PrintInt x)        = print x
+
+
+data Program a = Done a
+               | More Command (Program a)
+
+runProgram :: Program a -> IO a
+runProgram (Done x)    = return x
+runProgram (More c px) = runCommand c >> runProgram px
+
+
+
+
+
+instance Functor Program where
+  fmap f (Done x)    = Done (f x)
+  fmap f (More c px) = More c (fmap f px)
+
+instance Applicative Program where
+  pure = Done
+  (<*>) = ap
+
+instance Monad Program where
+  Done x    >>= f = f x
+  More c px >>= f = More c (px >>= f)
+
+
+
+-- like threadDelay, but using seconds instead of microseconds
+sleep :: Double -> IO ()
+sleep seconds = threadDelay $ round $ seconds * 1000 * 1000
 
 slowAdd :: Int -> Int -> IO Int
 slowAdd x1 x2 = do
@@ -17,19 +50,5 @@ slowAdd x1 x2 = do
 asyncAdd :: Int -> Int -> (Int -> IO ()) -> IO ()
 asyncAdd x1 x2 cc = void $ forkIO $ slowAdd x1 x2 >>= cc
 
-syncAdd :: Int -> Int -> IO Int
-syncAdd x1 x2 = do
-  var <- newEmptyMVar
-  asyncAdd x1 x2 (putMVar var)
-  takeMVar var
-
-
-
-
-
-
-
-
--- like threadDelay, but using seconds instead of microseconds
-sleep :: Double -> IO ()
-sleep seconds = threadDelay $ round $ seconds * 1000 * 1000
+main :: IO ()
+main = return ()
