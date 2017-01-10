6671e4703b8fb06e890a72ea3e5c6782dea4937c
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-10 09:00:42 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,23 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
-import Prelude hiding ((||))
+import Control.Concurrent
 
-(||) :: Bool -> Bool -> Bool
-True  || _    = True
-_     || True = True
-_     || _    = False
+parOr :: Bool -> Bool -> IO Bool
+parOr b1 b2 = raceM (return $ b1 || b2)
+                    (return $ b2 || b1)
+
+raceM :: IO a -> IO a -> IO a
+raceM ioX1 ioX2 = do
+  var <- newEmptyMVar
+  _ <- forkIO $ do !x1 <- ioX1; putMVar var x1
+  _ <- forkIO $ do !x2 <- ioX2; putMVar var x2
+  readMVar var
 
 main :: IO ()
 main = do
   putStrLn "go!"
-  print $ (fib 30 > 100) || (fib 40 > 100)
-  print $ (fib 40 > 100) || (fib 30 > 100)
-
-
-
-
-
-
-
-
+  print =<< parOr (fib 30 > 100) (fib 40 > 100)
+  print =<< parOr (fib 40 > 100) (fib 30 > 100)
 
 
 
