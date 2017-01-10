22dd299e54e6a7723b70b8086362f3a036ec08b0
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-10 14:12:31 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,61 +1,30 @@
-
-
-
-
-
-
-
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  Taming Asynchronous APIs using ContT       --
-        --                                             --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        --                                             --
-        -------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module Main where
+import Control.Concurrent
 
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = do
+  putStrLn "2 + 2 = ?"; slowAdd 2 2 >>= print
+  putStrLn "3 + 3 = ?"; slowAdd 3 3 >>= print
+  putStrLn "4 + 4 = ?"; slowAdd 4 4 >>= print
+
+
+slowAdd :: Int -> Int -> IO Int
+slowAdd x1 x2 = do
+  sleep 0.35
+  return $ x1 + x2
+
+
+
+
+
+
+
+
+
+
+
+
+-- like threadDelay, but using seconds instead of microseconds
+sleep :: Double -> IO ()
+sleep seconds = threadDelay $ round $ seconds * 1000 * 1000
