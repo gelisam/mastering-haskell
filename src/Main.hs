d320142667f746c7f869e8c592f1c063c5d41ed7
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-10 09:51:32 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,15 @@
 module Main where
-import Numeric.Natural
 
--- IVar with double put
-data CmdI a = Read   -- block until Put
-            | Put a  -- conflicts with (Put x') if x /= x'
-
--- (Bool, Bool)
-data CmdB = PutFst Bool  -- conflicts with (PutFst b') if b /= b'
-          | PutSnd Bool  -- conflicts with (PutSnd b') if b /= b'
-          | ReadFst      -- block until (PutFst b)
-          | ReadSnd      -- block until (PutSnd b)
-          | ReadAnd      -- block until one False or two Trues
-          | ReadOr       -- block until one True or two Falses
-
--- Incrementing counter
-data CmdF = Freeze
-          | WaitUntil Natural    -- block until value is reached
-          | IncrementBy Natural  -- error if after Freeze
-
--- Map k a
-data CmdL k a = Lookup k    -- block until (Insert k)
-              | Insert k a  -- conflicts with (Insert k' x')
-                            -- if k == k' but x /= x'
-
-
-
-
-
-
-
+-- do before             do before
+--                          
+--    ...                   ...
+--    
+--                          increment
+--    x1 <- freeze
+--    
+--    ...                   ...
+--    
+--    after                 after
 
 
 
