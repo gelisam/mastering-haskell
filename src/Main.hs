bed87cc15f47e3412e55a1768cf38f1414a863b4
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-10 10:56:37 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,36 @@
 module Main where
 import Control.Concurrent
+import Control.Exception.Base
 import Control.Monad
+import Data.Typeable
 
--- like threadDelay, but using seconds instead of microseconds
-sleep :: Double -> IO ()
-sleep seconds = threadDelay $ round $ seconds * 1000 * 1000
+
 
 main :: IO ()
 main = do
   var <- newEmptyMVar
-  _ <- forkIO $ do
+  _ <- flip forkFinally (putMVar var) $ do
     replicateM_ 4 $ do
       sleep 0.5
       putStrLn "thread"
       undefined  -- throws "ErrorCall"
-    putMVar var (42 :: Int)
+    return (42 :: Int)
   
   sleep 0.25
   
   replicateM_ 2 $ do
     sleep 0.5
     putStrLn "main"
-  print =<< takeMVar var
+  print . either throw id =<< takeMVar var
+
+
+
+
+
+-- See Volume 1, Video 1.3
+typeOfException :: SomeException -> String
+typeOfException (SomeException e) = show (typeOf e)
+
+-- like threadDelay, but using seconds instead of microseconds
+sleep :: Double -> IO ()
+sleep seconds = threadDelay $ round $ seconds * 1000 * 1000
