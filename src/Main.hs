16eb98c30fc0dbf3a60f614c73ac917e5924d473
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-12 13:32:40 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 import Control.Concurrent
-
+import Control.Monad
 
 main :: IO ()
 main = do
-  putStrLn "2 + 2 = ?"; slowAdd 2 2 >>= print
-  putStrLn "3 + 3 = ?"; slowAdd 3 3 >>= print
-  putStrLn "4 + 4 = ?"; slowAdd 4 4 >>= print
-
+  putStrLn "2 + 2 = ?"; void $ forkIO $ slowAdd 2 2 >>= print
+  putStrLn "3 + 3 = ?"; void $ forkIO $ slowAdd 3 3 >>= print
+  putStrLn "4 + 4 = ?"; void $ forkIO $ slowAdd 4 4 >>= print
+  forever $ sleep 1
 
 slowAdd :: Int -> Int -> IO Int
 slowAdd x1 x2 = do
