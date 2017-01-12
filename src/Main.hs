0e30869a74d4d11f86da59ab6826ae3ce7d6432c
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-12 12:04:21 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,17 @@
-{-# LANGUAGE BangPatterns #-}
 module Main where
-import Control.Concurrent
+import Control.Concurrent.Async
+import Control.Exception.Base
 
 fibPair :: (Integer, Integer)
 fibPair = (fib 10, fib 20)
 
-parPair :: (a, b) -> Parallel (a, b)
-parPair (x, y) = (,) <$> pure x
-                     <*> pure y
+parPair :: (a, b) -> Concurrently (a, b)
+parPair (x, y) = (,) <$> Concurrently (evaluate x)
+                     <*> Concurrently (evaluate y)
 
 main :: IO ()
 main = do
-  r <- runParallel $ parPair fibPair
+  r <- runConcurrently $ parPair fibPair
   print r
 
 
@@ -27,22 +27,3 @@ fib :: Int -> Integer
 fib 0 = 1
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
-
-
-
-newtype Parallel a = Parallel { runParallel :: IO a }
-
-instance Functor Parallel where
-  fmap f (Parallel ioX) = Parallel $ do !x <- ioX
-                                        return (f x)
-
-instance Applicative Parallel where
-  pure = Parallel . pure
-  Parallel ioF <*> Parallel ioX = Parallel $ do
-    varF <- newEmptyMVar
-    varX <- newEmptyMVar
-    _ <- forkIO $ do !f <- ioF
-                     putMVar varF f
-    _ <- forkIO $ do !x <- ioX
-                     putMVar varX x
-    takeMVar varF <*> takeMVar varX
