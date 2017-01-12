b6251dfa1b9dd1369316709857334c1c6bd718e5
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-12 10:44:46 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,61 +1,35 @@
-
-
-
-
-
-
-
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  Opportunistic Concurrency using Async      --
-        --                                             --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        --                                             --
-        -------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module Main where
+import Control.Concurrent
+import Control.Exception.Base
+import Control.Monad
+import Data.Typeable
 
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = do
+  var <- newEmptyMVar
+  _ <- flip forkFinally (putMVar var) $ do
+    replicateM_ 4 $ do
+      sleep 0.5
+      putStrLn "thread"
+      undefined  -- throws "ErrorCall"
+    return (42 :: Int)
+  
+  sleep 0.25
+  
+  replicateM_ 2 $ do
+    sleep 0.5
+    putStrLn "main"
+  print . either typeOfException show =<< takeMVar var
+
+
+
+
+
+-- See Volume 1, Video 1.3
+typeOfException :: SomeException -> String
+typeOfException (SomeException e) = show (typeOf e)
+
+-- like threadDelay, but using seconds instead of microseconds
+sleep :: Double -> IO ()
+sleep seconds = threadDelay $ round $ seconds * 1000 * 1000
