599d61cc4f5429103358d8f524ce030f4b1c8148
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-12 12:06:58 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,12 @@
 module Main where
+import Control.Concurrent
 import Control.Concurrent.Async
-import Control.Exception.Base
-
-fibPair :: (Integer, Integer)
-fibPair = (fib 10, fib 20)
-
-parPair :: (a, b) -> Concurrently (a, b)
-parPair (x, y) = (,) <$> Concurrently (evaluate x)
-                     <*> Concurrently (evaluate y)
+import Control.Monad
 
 main :: IO ()
 main = do
-  r <- runConcurrently $ parPair fibPair
-  print r
-
-
-
-
-
-
-
-
-
-
-fib :: Int -> Integer
-fib 0 = 1
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+  rs <- replicateM 10 $ do
+    var <- newMVar (0 :: Int)
+    runConcurrently $ const <$> Concurrently (readMVar var)
+                            <*> Concurrently (swapMVar var 1)
+  print rs
