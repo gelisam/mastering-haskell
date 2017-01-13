c77d2c9d2008f983a046fb54975c7e9b9bab0faf
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-13 16:40:46 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -3,13 +3,13 @@ module Main where
 import Control.Monad.Trans.Cont
 import Text.Printf
 
-mkString :: Monad m => m String
+mkString :: Cont r String
 mkString = printf "%d-%d-%d" <$> mkInt 0 <*> mkInt 1 <*> mkInt 2
 
-mkInt :: Monad m => Int -> m Int
+mkInt :: Int -> Cont r Int
 mkInt x = round <$> mkDouble (fromIntegral x)
 
-mkDouble :: Monad m => Double -> m Double
+mkDouble :: Double -> Cont r Double
 mkDouble x = return $ 1.5 * x
 
 
