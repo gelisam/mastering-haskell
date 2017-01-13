4f165f3ead6ff4ac369f920d8dad475fdbbb90d6
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-13 16:40:10 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,23 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
-
 import Control.Monad.Trans.Cont
 import Text.Printf
 
-mkString :: Cont String String
+mkString :: ContT String IO String
 mkString = printf "%d-%d-%d" <$> mkInt 0 <*> mkInt 1 <*> mkInt 2
 
-mkInt :: Int -> Cont String Int
+mkInt :: Int -> ContT String IO Int
 mkInt x = round <$> mkDouble (fromIntegral x)
 
-mkDouble :: Double -> Cont String Double
-mkDouble 1 = cont $ \cc -> head
-                         $ filter (== "0-3-3")
-                         $ map cc [0,0.1..]
+mkDouble :: Double -> ContT String IO Double
+mkDouble 1 = ContT $ \(cc :: Double -> IO String) -> do
+                       let loop i = do
+                             printf "trying %.1f\n" i
+                             r <- cc i
+                             if r == "0-3-3" then return r
+                                             else loop (i+0.1)
+                       loop 0
 mkDouble x = return $ 1.5 * x
 
 main :: IO ()
-main = print (evalCont mkString)
+main = print =<< evalContT mkString
