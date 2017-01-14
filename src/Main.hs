a0a6c88a891baeb615bd72b940d0fb8d424a1c4e
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-14 10:39:24 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 import Data.List
-
-
+import Debug.Trace
+import Text.Printf
 
 
 main :: IO ()
@@ -10,5 +10,10 @@ main = print $ qsort [4,2,7,8,1,6,3]
 qsort :: [Int] -> [Int]
 qsort []     = []
 qsort [x]    = [x]
-qsort (x:xs) = let (xsLT, xsGE) = partition (< x) xs
+qsort (x:xs) = trace (printf "%s: partition (< %s) %s"
+                       (show (x:xs)) (show x) (show xs))
+             $ let (xsLT, xsGE) = partition (`noisyLT` x) xs
                in qsort xsLT ++ [x] ++ qsort xsGE
+
+noisyLT :: Int -> Int -> Bool
+noisyLT x y = trace (printf "%d < %d" x y) (x < y)
