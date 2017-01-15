9ba1afbb1feda9c8c5d58ccac7d9c9c57571b838
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-15 14:39:04 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,22 @@ data AsyncList a = Cons a (Async (AsyncList a))
 data MsgQueue  a = Cons' a (IVar (MsgQueue a))
 
 
+newtype AVar a = AVar { getAVar :: Either a (Async a)
+                      }
 
+instance Functor AVar where
+  fmap f (AVar (Left x))       = AVar (Left (f x))
+  fmap f (AVar (Right asyncX)) = AVar (Right (fmap f asyncX))
+
+pureA :: a -> AVar a
+pureA = AVar . Left
+
+asyncA :: IO a -> IO (AVar a)
+asyncA = fmap (AVar . Right) . async
+
+waitA :: AVar a -> IO a
+waitA (AVar (Left x))       = return x
+waitA (AVar (Right asyncX)) = wait asyncX
 
 
 
