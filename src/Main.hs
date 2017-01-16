fcb051e119630fdfe1231d31b75563ff4951b0b5
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-16 18:29:44 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,38 +1,79 @@
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 import Control.Concurrent
+import Control.Monad
 
-producer :: MVar [Int] -> IO ()
-producer var = go 0
+data BoundedBuffer a = BoundedBuffer { buffer      :: MVar [a]
+                                     , okToProduce :: Signal
+                                     , okToConsume :: Signal
+                                     }
+
+producer :: BoundedBuffer Int -> IO ()
+producer (BoundedBuffer {..}) = go 0
   where
     go x = do sleep 0.3
-              modifyMVar_ var $ \xs -> do
+              full <- modifyMVar buffer $ \xs -> do
                 let xs' = xs ++ [x]
+                let full = length xs' == 4
                 putStrLn $ "PRODUCER " ++ show xs'
-                return xs'
+                return (xs', full)
+              signal okToConsume
+              when full $ do
+                block okToProduce
               go (x+1)
 
-consumer :: MVar [Int] -> IO ()
-consumer var = go
+
+
+
+
+
+
+
+
+newBoundedBuffer :: IO (BoundedBuffer a)
+newBoundedBuffer = BoundedBuffer <$> newMVar []
+                                 <*> newSignal
+                                 <*> newSignal
+
+consumer :: BoundedBuffer Int -> IO ()
+consumer (BoundedBuffer {..}) = go
   where
     go = do sleep 0.4
-            modifyMVar_ var $ \xs -> do
+            empty <- modifyMVar buffer $ \xs -> do
               let xs' = drop 1 xs
+              let empty = length xs' == 0
               putStrLn $ "CONSUMER " ++ show xs'
-              return xs'
+              return (xs', empty)
+            signal okToProduce
+            when empty $ do
+              block okToConsume
             go
 
 
 
+type Signal = MVar ()
 
+newSignal :: IO Signal
+newSignal = newEmptyMVar
 
+block :: Signal -> IO ()
+block cond = do _ <- tryTakeMVar cond
+                takeMVar cond
+                signal cond
+
+signal :: Signal -> IO ()
+signal cond = do _ <- tryPutMVar cond ()
+                 return ()
 
 
 
 main :: IO ()
 main = do
-  var <- newMVar []
-  _ <- forkIO $ producer var
-  _ <- forkIO $ consumer var
+  bounderBuffer <- BoundedBuffer <$> newMVar []
+                                 <*> newSignal
+                                 <*> newSignal
+  _ <- forkIO $ producer bounderBuffer
+  _ <- forkIO $ consumer bounderBuffer
   
   let loop = do sleep 1
                 loop
