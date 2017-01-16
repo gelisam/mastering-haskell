2e875af297a8a41b4c2446c68703b0bcac3f2f14
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-16 16:42:29 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,25 @@
 module Main where
+import Control.Concurrent
 
--- do before             do before
---                          
---    ...                   ...
---    
---                          signal cond
---    block cond
---    BLOCKS
---    
---                          ...
---    
---                          after
+producer :: MVar [Int] -> IO ()
+producer var = go 0
+  where
+    go x = do sleep 0.3
+              modifyMVar_ var $ \xs -> do
+                let xs' = xs ++ [x]
+                putStrLn $ "PRODUCER " ++ show xs'
+                return xs'
+              go (x+1)
 
-
-
-
-
-
-
-
+consumer :: MVar [Int] -> IO ()
+consumer var = go
+  where
+    go = do sleep 0.4
+            modifyMVar_ var $ \xs -> do
+              let xs' = drop 1 xs
+              putStrLn $ "CONSUMER " ++ show xs'
+              return xs'
+            go
 
 
 
@@ -28,4 +29,17 @@ module Main where
 
 
 main :: IO ()
-main = return ()
+main = do
+  var <- newMVar []
+  _ <- forkIO $ producer var
+  _ <- forkIO $ consumer var
+  
+  let loop = do sleep 1
+                loop
+  loop
+
+
+
+-- like threadDelay, but using seconds instead of microseconds
+sleep :: Double -> IO ()
+sleep seconds = threadDelay $ round $ seconds * 1000 * 1000
