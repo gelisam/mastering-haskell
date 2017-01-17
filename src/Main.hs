a601d963c91c9c04354893cf749a691f9f94e735
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-17 08:33:37 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,23 @@
 module Main where
+import Control.Concurrent
+import Control.Monad
 
--- do before             do before
---                          
---    ...                   ...
---                          reset  okToConsume
---                          signal okToProduce
---    reset  okToProduce    
---    signal okToConsume    
---    block  okToProduce    block  okToConsume
---    BLOCKS                
---                          ...
---    
---                          after
+
+type Signal = MVar ()
+
+newSignal :: IO Signal
+newSignal = newEmptyMVar
+
+reset :: Signal -> IO ()
+reset = void . tryTakeMVar
+
+block :: Signal -> IO ()
+block var = takeMVar var >> signal var
+
+signal :: Signal -> IO ()
+signal = void . flip tryPutMVar ()
+
+
 
 
 
