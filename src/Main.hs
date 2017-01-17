6b42b51bcc5ae65069406f2d3a09df56fb965a78
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-17 12:14:49 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,16 @@
 module Main where
-import Control.Concurrent
-import Control.Monad
 
-
-waitUntil :: (a -> Bool) -> MVar a -> IO ()
-waitUntil p var = do x <- readMVar var
-                     unless (p x) $ do yield
-                                       waitUntil p var
-
-
-
+-- do before             do before
+--                          
+--    ...                   ...
+--    
+--    block cond
+--    BLOCKS
+--                          signal cond
+--    UNBLOCKS
+--    ...                   ...
+--    
+--    after                 after
 
 
 
