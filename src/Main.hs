95005e3a0a415ce77e23e4deeb7cdb6a5725fb6e
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-17 12:55:32 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,53 @@
+{-# LANGUAGE LambdaCase, RecordWildCards, TupleSections #-}
 module Main where
 import Control.Concurrent
 import Control.Monad
+import Data.IORef
+
+data MyMVar a = MyMVar { payload  :: IORef (Maybe a)
+                       , okToPut  :: Signal
+                       , okToTake :: Signal
+                       }
+
+tryTakeMyMVar :: MyMVar a -> IO (Maybe a)
+tryTakeMyMVar (MyMVar {..}) = do
+  r <- atomicModifyIORef payload (Nothing,)
+  when (isJust r) $ signal okToPut
+  return r
+
+takeMyMVar :: MyMVar a -> IO a
+takeMyMVar var@(MyMVar {..}) = do
+  reset okToTake
+  tryTakeMyMVar var >>= \case
+    Just x  -> do return x
+    Nothing -> do block okToTake
+                  takeMyMVar var
+
+
+
+
+
+newEmptyMyMVar :: IO (MyMVar a)
+newEmptyMyMVar = MyMVar <$> newIORef Nothing
+                        <*> newSignal
+                        <*> newSignal
+
+tryPutMyMVar :: MyMVar a -> a -> IO Bool
+tryPutMyMVar (MyMVar {..}) x = do
+  r <- atomicModifyIORef payload $ \case
+    Nothing -> (Just x, True)
+    Just x' -> (Just x', False)
+  when r $ signal okToTake
+  return r
+
+putMyMVar :: MyMVar a -> a -> IO ()
+putMyMVar var@(MyMVar {..}) x = do
+  reset okToPut
+  tryPutMyMVar var x >>= \case
+    True  -> return ()
+    False -> do block okToPut
+                putMyMVar var x
+
 
 
 type Signal = MVar ()
@@ -12,27 +59,26 @@ reset :: Signal -> IO ()
 reset = void . tryTakeMVar
 
 block :: Signal -> IO ()
-block var = takeMVar var >> signal var
+block = takeMVar
 
 signal :: Signal -> IO ()
 signal = void . flip tryPutMVar ()
 
+takeWhen :: MVar a -> (a -> Bool) -> Signal -> IO a
+takeWhen var p cond = do x <- takeMVar var
+                         let satisfied = p x
+                         if satisfied
+                         then return x
+                         else do reset cond
+                                 putMVar var x
+                                 block cond
+                                 takeWhen var p cond
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+isJust :: Maybe a -> Bool
+isJust (Just _) = True
+isJust Nothing  = False
 
 main :: IO ()
 main = return ()
