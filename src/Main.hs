f0d2f167c0474ca0b301d336cf78b2ca598c18ab
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-18 17:41:37 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,35 @@ module Main where
 import Control.Concurrent
 import Control.Concurrent.Async
 
+traceAppendMVar :: MVar [String] -> String -> IO ()
+traceAppendMVar var s = do
+  putStrLn s
+  appendMVar var s
+
+
+
+
+main :: IO ()
+main = do
+  lock <- newLock
+  var <- newMVar []
+  tA <- async $ withLock lock $ do traceAppendMVar var "A"
+                                   traceSleep "A" 0.5
+                                   traceAppendMVar var "AA"
+  tB <- async $ withLock lock $ do traceAppendMVar var "B"
+                                   traceSleep "B" 0.5
+                                   traceAppendMVar var "BB"
+  mapM_ wait [tA,tB]
+
+
+
+traceSleep :: String -> Double -> IO ()
+traceSleep t seconds = do
+  putStrLn $ t ++ " is sleeping for " ++ show seconds ++ " seconds"
+  sleep seconds
+
+
+
 type Lock = MVar ()
 
 newLock :: IO Lock
@@ -9,17 +38,6 @@ newLock = newMVar ()
 
 withLock :: Lock -> IO a -> IO a
 withLock lock body = withMVar lock $ \_ -> body
-
-main :: IO ()
-main = printUniqueResults [] $ do
-  lock <- newLock
-  var <- newMVar []
-  tA <- async $ withLock lock $ do appendMVar var "A"
-                                   appendMVar var "AA"
-  tB <- async $ withLock lock $ do appendMVar var "B"
-                                   appendMVar var "BB"
-  mapM_ wait [tA,tB]
-  readMVar var
 
 
 
@@ -31,3 +49,9 @@ printUniqueResults seen body = do
 
 appendMVar :: MVar [a] -> a -> IO ()
 appendMVar var x = modifyMVar_ var (return . (++ [x]))
+
+
+
+-- like threadDelay, but using seconds instead of microseconds
+sleep :: Double -> IO ()
+sleep seconds = threadDelay $ round $ seconds * 1000 * 1000
