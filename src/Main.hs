011cca6262c6dbd40946182cdc4c6dfc62913b15
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-18 20:14:01 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,50 +1,29 @@
 module Main where
-import Control.Concurrent
 import Control.Concurrent.Async
+import Control.Concurrent.STM
 
-traceReadMVar :: MVar (String, Lock, MVar a) -> IO (Lock, MVar a)
-traceReadMVar var = do
-  (s, l, v) <- readMVar var
-  putStrLn $ "got var" ++ s
-  return (l, v)
+appendTVar :: TVar [a] -> a -> STM ()
+appendTVar var x = modifyTVar var (++ [x])
+
+
+
+
+
+
+
+
 
 
 
 main :: IO ()
-main = do
-  lock1 <- newLock; var1 <- newMVar []
-  lock2 <- newLock; var2 <- newMVar []
-  tA <- async $ withLock lock1 $ do traceAppendMVar var1 "A"
-                                    traceSleep "A" 0.5
-                                    withLock lock2 $ do
-                                      traceAppendMVar var2 "AA"
-  tB <- async $ withLock lock2 $ do traceAppendMVar var2 "B"
-                                    traceSleep "B" 0.5
-                                    withLock lock1 $ do
-                                      traceAppendMVar var1 "BB"
+main = printUniqueResults [] $ do
+  var <- atomically $ newTVar []
+  tA <- async $ atomically $ do appendTVar var "A"
+                                appendTVar var "AA"
+  tB <- async $ atomically $ do appendTVar var "B"
+                                appendTVar var "BB"
   mapM_ wait [tA,tB]
-
-
-
-traceAppendMVar :: MVar [String] -> String -> IO ()
-traceAppendMVar var s = do
-  putStrLn s
-  appendMVar var s
-
-traceSleep :: String -> Double -> IO ()
-traceSleep t seconds = do
-  putStrLn $ t ++ " is sleeping for " ++ show seconds ++ " seconds"
-  sleep seconds
-
-
-
-type Lock = MVar ()
-
-newLock :: IO Lock
-newLock = newMVar ()
-
-withLock :: Lock -> IO a -> IO a
-withLock lock body = withMVar lock $ \_ -> body
+  atomically $ readTVar var
 
 
 
@@ -53,12 +32,3 @@ printUniqueResults seen body = do
   x <- body
   if x `elem` seen then printUniqueResults seen body
                    else print x >> printUniqueResults (x:seen) body
-
-appendMVar :: MVar [a] -> a -> IO ()
-appendMVar var x = modifyMVar_ var (return . (++ [x]))
-
-
-
--- like threadDelay, but using seconds instead of microseconds
-sleep :: Double -> IO ()
-sleep seconds = threadDelay $ round $ seconds * 1000 * 1000
