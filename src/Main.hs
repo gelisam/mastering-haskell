a6f85aa62e63e2c2ad167419f2ae929b53e23d74
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-22 18:59:36 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,54 +1,9 @@
+module Main where
+import Control.Concurrent.Async
+import Control.Monad.Trans.State
 
-
-
-
-
-
-
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  Effect Tracking and Concurrency            --
-        --                                             --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        --                                             --
-        -------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+appendState :: a -> State [a] ()
+appendState x = modify (++ [x])
 
 
 
@@ -58,4 +13,28 @@
 
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = printUniqueResults [] $ do
+  let xs = []
+  xsA <- async $ return $ flip execState xs $ do appendState "A"
+                                                 appendState "AA"
+  xsB <- async $ return $ flip execState xs $ do appendState "B"
+                                                 appendState "BB"
+  (++) <$> wait xsA <*> wait xsB
+
+
+
+
+
+
+
+
+
+
+
+
+
+printUniqueResults :: (Show a, Eq a) => [a] -> IO a -> IO ()
+printUniqueResults seen body = do
+  x <- body
+  if x `elem` seen then printUniqueResults seen body
+                   else print x >> printUniqueResults (x:seen) body
