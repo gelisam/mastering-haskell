2371eeec748e9f6e259a966326f28a8df57f7091
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-24 11:17:34 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,61 +1,22 @@
+module Main where
+import Control.Concurrent
+import Control.Concurrent.Async
 
+printUniqueResults :: (Show a, Eq a) => [a] -> IO a -> IO ()
+printUniqueResults seen body = do
+  x <- body
+  if x `elem` seen then printUniqueResults seen body
+                   else print x >> printUniqueResults (x:seen) body
 
-
-
-
-
-
-
-
-
-        -------------------------------------------------
-        --                                             --
-        --  Automatic Signaling using TVars            --
-        --                                             --
-        --                         by Samuel Gélineau  --
-        --                         published by Packt  --
-        --                                             --
-        -------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+appendMVar :: MVar [a] -> a -> IO ()
+appendMVar var x = modifyMVar_ var (return . (++ [x]))
 
 main :: IO ()
-main = putStrLn "Welcome to the course!"
+main = printUniqueResults [] $ do
+  var <- newMVar []
+  tA <- async $ do appendMVar var "A"
+                   appendMVar var "AA"
+  tB <- async $ do appendMVar var "B"
+                   appendMVar var "BB"
+  mapM_ wait [tA,tB]
+  readMVar var
