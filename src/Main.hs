e8152697a716a0b95f9fe9dee3cf7baf67ae619b
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-23 20:44:59 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,32 +1,27 @@
 module Main where
-import Control.Concurrent.Async
 import Data.IORef
 
 
 
-myAtomicModifyIORef :: IORef a -> (a -> (a, b)) -> IO b
-myAtomicModifyIORef ref f = do
-  (x', y) <- f <$> readIORef ref
-  writeIORef ref x'
-  return y
 
-
-
+compareAndSwap :: Eq a => IORef a -> a -> a -> IO Bool
+compareAndSwap ref expected replacement = do
+  actual <- readIORef ref
+  if actual == expected then do writeIORef ref replacement
+                                return True
+                        else return False
 
 
 
 main :: IO ()
-main = printUniqueResults [] $ do
-  ref <- newIORef (0 :: Int)
-  tA <- async $ myAtomicModifyIORef ref (\x -> (x+1, ()))
-  tB <- async $ myAtomicModifyIORef ref (\x -> (x+1, ()))
-  mapM_ wait [tA,tB]
-  readIORef ref
+main = do
+  ref <- newIORef (42 :: Int)
+  
+  print =<< compareAndSwap ref 5 10
+  print =<< readIORef ref
+  
+  print =<< compareAndSwap ref 42 50
+  print =<< readIORef ref
 
 
 
-printUniqueResults :: (Show a, Eq a) => [a] -> IO a -> IO ()
-printUniqueResults seen body = do
-  x <- body
-  if x `elem` seen then printUniqueResults seen body
-                   else print x >> printUniqueResults (x:seen) body
