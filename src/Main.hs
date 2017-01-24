28ea644da5be4a4d098b5bbfb4b13a81b86da618
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-24 11:16:33 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,27 @@ module Main where
 import Control.Concurrent
 import Control.Concurrent.Async
 
+type Lock = MVar ()
+
+newLock :: IO Lock
+newLock = newMVar ()
+
+withLock :: Lock -> IO a -> IO a
+withLock lock body = withMVar lock $ \_ -> body
+
+main :: IO ()
+main = printUniqueResults [] $ do
+  lock <- newLock
+  var <- newMVar []
+  tA <- async $ withLock lock $ do appendMVar var "A"
+                                   appendMVar var "AA"
+  tB <- async $ withLock lock $ do appendMVar var "B"
+                                   appendMVar var "BB"
+  mapM_ wait [tA,tB]
+  readMVar var
+
+
+
 printUniqueResults :: (Show a, Eq a) => [a] -> IO a -> IO ()
 printUniqueResults seen body = do
   x <- body
@@ -10,13 +31,3 @@ printUniqueResults seen body = do
 
 appendMVar :: MVar [a] -> a -> IO ()
 appendMVar var x = modifyMVar_ var (return . (++ [x]))
-
-main :: IO ()
-main = printUniqueResults [] $ do
-  var <- newMVar []
-  tA <- async $ do appendMVar var "A"
-                   appendMVar var "AA"
-  tB <- async $ do appendMVar var "B"
-                   appendMVar var "BB"
-  mapM_ wait [tA,tB]
-  readMVar var
