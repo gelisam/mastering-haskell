4cd0efec1e4f79ea64cbf4726ea9d587e1f35c9b
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-25 14:54:22 -0500

    new slide

--- src/Main.c
+++ src/Main.c
@@ -1,5 +1,5 @@
 
-int inline_c_Main_0_47f9490fc454deee04475f1e17d15d9b4fb38fec(int * ptr_inline_c_0, int expected_inline_c_1, int replacement_inline_c_2) {
+int inline_c_Main_0_c62503c62e82f10485c7ce8c6a84f9ad27426c2a(void ** ptr_inline_c_0, void * expected_inline_c_1, void * replacement_inline_c_2) {
 
          return __sync_bool_compare_and_swap(
            ptr_inline_c_0,
--- src/Main.hs
+++ src/Main.hs
@@ -1,32 +1,27 @@
 {-# LANGUAGE QuasiQuotes, RankNTypes, TemplateHaskell #-}
 module Main where
-import Control.Concurrent.Async
 import Foreign
 import qualified Language.C.Inline as C
 
-compareAndSwap :: Ptr C.CInt -> C.CInt -> C.CInt -> IO Bool
+
+compareAndSwap :: Ptr (Ptr ()) -> Ptr () -> Ptr () -> IO Bool
 compareAndSwap ptr expected replacement = do
   r <- [C.block| int {
          return __sync_bool_compare_and_swap(
-           $(int* ptr),
-           $(int expected),
-           $(int replacement)
+           $(void** ptr),
+           $(void* expected),
+           $(void* replacement)
          );
        }|]
   return (r /= 0)
 
 
 
+
+
+
+
+
+
 main :: IO ()
-main = printUniqueResults [] $ with 0 $ \ptr -> do
-  tA <- async $ compareAndSwap ptr 0 1
-  tB <- async $ compareAndSwap ptr 0 1
-  mapM wait [tA,tB]
-
-
-
-printUniqueResults :: (Show a, Eq a) => [a] -> IO a -> IO ()
-printUniqueResults seen body = do
-  x <- body
-  if x `elem` seen then printUniqueResults seen body
-                   else print x >> printUniqueResults (x:seen) body
+main = return ()
