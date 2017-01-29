3614aa867e98aa4a156041f2c45ca78467a39c79
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-29 15:34:28 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,21 @@
+{-# LANGUAGE BangPatterns, GADTs #-}
 module Main where
 import Data.Time
 
+runSignal :: (Int -> Int) -> (Int -> Int) -> Signal a -> a
+runSignal delayedVisitCount delayedPopupCount = go 0
+  where
+    go :: Int -> Signal a -> a
+    go _     (Pure x)   = x
+    go delay (Ap cc sx) = (go delay cc) (runSignalF delay sx)
+    
+    runSignalF :: Int -> SignalF a -> a
+    runSignalF delay VisitCount         = delayedVisitCount delay
+    runSignalF delay PopupCount         = delayedPopupCount delay
+    runSignalF delay (TimeDelayed d sx) = go (delay + d) sx
 
 
 
-
-
-
-
-
-
-
-
-
-
-delayedVisitCount :: User -> UTCTime -> IO (Int -> Int)
-delayedVisitCount = undefined
 
 rpcDelayedVisitCount :: User -> UTCTime -> Int -> IO Int
 rpcDelayedVisitCount = rpc host port "delayedVisitCount"
@@ -50,6 +50,28 @@ data Handle = Handle
 
 rpc :: RPC b => String -> Int -> String -> b
 rpc _ _ _ = clientSide $ return Handle
+
+
+
+-- ((f <$> fx) <*> fy) <*> fz
+data FreeAp f a where
+  Pure :: a -> FreeAp f a
+  Ap   :: FreeAp f (e -> a) -> f e -> FreeAp f a
+
+type Signal a = FreeAp SignalF a
+data SignalF a where
+  VisitCount  :: SignalF Int
+  PopupCount  :: SignalF Int
+  TimeDelayed :: Int -> Signal a -> SignalF a
+
+visitCount :: Signal Int
+visitCount = Ap (Pure id) VisitCount
+
+popupCount :: Signal Int
+popupCount = Ap (Pure id) PopupCount
+
+timeDelayed :: Int -> Signal a -> Signal a
+timeDelayed days bx = Ap (Pure id) (TimeDelayed days bx)
 
 
 
