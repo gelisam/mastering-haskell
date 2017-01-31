927a6b07821fb1353a93b6a3abb7001f53ace8e8
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-31 16:28:22 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,38 @@
+{-# LANGUAGE GADTs #-}
 module Main where
 
-data Signal a
+-- ((f <$> fx) <*> fy) <*> fz
+data FreeAp f a where
+  Pure :: a -> FreeAp f a
+  Ap   :: FreeAp f (e -> a) -> f e -> FreeAp f a
 
-visitCount  :: Signal Int
-popupCount  :: Signal Int
+type Signal a = FreeAp SignalF a
+data SignalF a where
+  VisitCount  :: SignalF Int
+  PopupCount  :: SignalF Int
+  TimeDelayed :: Int -> Signal a -> SignalF a
+
+visitCount :: Signal Int
+visitCount = Ap (Pure id) VisitCount
+
+popupCount :: Signal Int
+popupCount = Ap (Pure id) PopupCount
 
 timeDelayed :: Int -> Signal a -> Signal a
+timeDelayed days bx = Ap (Pure id) (TimeDelayed days bx)
+
+
+
+instance Functor (FreeAp f) where
+  fmap f (Pure x)   = Pure (f x)
+  fmap f (Ap fs fe) = Ap (fmap (fmap f) fs) fe
+
+instance Applicative (FreeAp f) where
+  pure = Pure
+  Pure f   <*> fx = fmap f fx
+  Ap fs fe <*> fx = Ap (flip <$> fs <*> fx) fe
+
+
 
 lastWeekVisitCount :: Signal Int
 lastWeekVisitCount = (-) <$> visitCount <*> timeDelayed 7 visitCount
@@ -20,31 +47,6 @@ recentPopup = (>) <$> popupCount <*> timeDelayed 2 popupCount
 shouldPopup :: Signal Bool
 shouldPopup = (&&) <$> shownInterest 
                    <*> (not <$> recentPopup)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-visitCount  = undefined
-popupCount  = undefined
-
-timeDelayed = undefined
-
-instance Functor Signal where
-  fmap = undefined
-
-instance Applicative Signal where
-  pure = undefined
-  (<*>) = undefined
 
 
 
