2c4258d4db3573d64bdf3358279754f6297db282
Samuel Gélineau <gelisam+github@gmail.com>
2017-01-31 15:33:50 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,40 @@
 module Main where
-
--- .---------------------------------------------.
--- | https://mynews.com/article/42            [x]|
--- |---------------------------------------------|
--- |                                             |
--- |  -- ----  -- ------- -- ----- -- ----- ---  |
--- |  --- -------- -+=============+ -- ------ -  |
--- |  -- --- - -----‖             ‖------- ----  | * 10+ visits
--- |  ----- ---     ‖  SUBSCRIBE  ‖              | * 3+ within 7 days
--- |                ‖    TODAY!   ‖              | * max 1 popup
--- |  - ---- -- ----‖             ‖- -- -------  |   every 48 hours
--- |  ---- --- - ---+=============+-- ----- ---  |
--- |  -- ---------                               |
--- |                                             |
--- '---------------------------------------------'
+import Data.IORef
+import Data.Time
+import qualified Data.MultiMap as MM
 
 
+type VisitTracker = IORef (MM.MultiMap User UTCTime)
+
+newVisitTracker :: IO VisitTracker
+newVisitTracker = newIORef MM.empty
+
+recordVisit :: VisitTracker -> User -> UTCTime -> IO ()
+recordVisit ref u t = do
+  modifyIORef ref $ MM.insert u t
+
+countVisits :: VisitTracker -> User -> IO Int
+countVisits ref u = do
+  length . (MM.! u) <$> readIORef ref
+
+countVisitsSince :: VisitTracker -> User -> UTCTime -> IO Int
+countVisitsSince ref u t = do
+  length . filter (>= t) . (MM.! u) <$> readIORef ref
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+data User = User deriving (Eq, Ord)
 
 
 
