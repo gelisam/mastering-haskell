4d0f7b9b20b33e42c4f8215c0ac1924beed0036a
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-02 17:50:25 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,60 +1,80 @@
+{-# LANGUAGE GADTs #-}
 module Main where
 
-data Signal a
+runSignal :: (Int -> IO Int) -> (Int -> IO Int) -> Signal a -> IO a
+runSignal delayedVisitCount delayedPopupCount = go 0
+  where
+    go :: Int -> Signal a -> IO a
+    go _     (Pure x)   = return x
+    go delay (Ap cc sx) = go delay cc <*> runSignalF delay sx
+    
+    runSignalF :: Int -> SignalF a -> IO a
+    runSignalF delay VisitCount          = delayedVisitCount delay
+    runSignalF delay PopupCount          = delayedPopupCount delay
+    runSignalF delay (TimeDelayed d sx)  = go (delay + d) sx
+    runSignalF delay (IOSignal delayedX) = delayedX delay
 
-visitCount  :: Signal Int
-popupCount  :: Signal Int
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data User = User deriving (Eq, Ord, Read, Show)
+
+
+data Handle = Handle
+
+
+
+-- ((f <$> fx) <*> fy) <*> fz
+data FreeAp f a where
+  Pure :: a -> FreeAp f a
+  Ap   :: FreeAp f (e -> a) -> f e -> FreeAp f a
+
+type Signal a = FreeAp SignalF a
+data SignalF a where
+  VisitCount  :: SignalF Int
+  PopupCount  :: SignalF Int
+  TimeDelayed :: Int -> Signal a -> SignalF a
+  IOSignal    :: (Int -> IO a) -> SignalF a
+
+visitCount :: Signal Int
+visitCount = Ap (Pure id) VisitCount
+
+popupCount :: Signal Int
+popupCount = Ap (Pure id) PopupCount
 
 timeDelayed :: Int -> Signal a -> Signal a
-ioSignal    :: (Int -> IO a) -> Signal a
+timeDelayed days bx = Ap (Pure id) (TimeDelayed days bx)
 
-lastWeekVisitCount :: Signal Int
-lastWeekVisitCount = (-) <$> visitCount <*> timeDelayed 7 visitCount
-
-shownInterest :: Signal Bool
-shownInterest = ioSignal rpcShownInterest
+ioSignal :: (Int -> IO a) -> Signal a
+ioSignal delayedX = Ap (Pure id) (IOSignal delayedX)
 
 
-recentPopup :: Signal Bool
-recentPopup = ioSignal rpcRecentPopup
+instance Functor (FreeAp f) where
+  fmap f (Pure x)   = Pure (f x)
+  fmap f (Ap fs fe) = Ap (fmap (fmap f) fs) fe
 
-shouldPopup :: Signal Bool
-shouldPopup = (&&) <$> shownInterest 
-                   <*> (not <$> recentPopup)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-visitCount  = undefined
-popupCount  = undefined
-
-timeDelayed = undefined
-ioSignal    = undefined
-
-instance Functor Signal where
-  fmap = undefined
-
-instance Applicative Signal where
-  pure = undefined
-  (<*>) = undefined
-
-
-
-rpcShownInterest :: Int -> IO Bool
-rpcShownInterest = undefined
-
-rpcRecentPopup :: Int -> IO Bool
-rpcRecentPopup = undefined
+instance Applicative (FreeAp f) where
+  pure = Pure
+  Pure f   <*> fx = fmap f fx
+  Ap fs fe <*> fx = Ap (flip <$> fs <*> fx) fe
 
 
 
