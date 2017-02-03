45672fec4e328be86c51c7900f3b65f9fd01d46f
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-03 17:25:03 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,14 @@
 module Main where
 
 
-
-
-
-
-
-
-
-
-
-runTransform :: Transform a (STM ())
-             -> IO a
-             -> (STM () -> IO ())
-             -> IO ()
-
-
-
-
-
-
-
-atomically :: STM a -> IO a
+--                         .--o          ,----o
+--                       /    ,---------o
+--                 .----o----o           '----o                            
+--               /            '---o                             
+--              o                     ,---o         
+--               \             ,-----o                                 
+--                 '----------o       '---o                         
+--                             '-----o                                     
 
 
 
@@ -33,41 +20,6 @@ atomically :: STM a -> IO a
 
 
 
-
-
-
-
-
-
-
-
-runTransform = undefined
-
-
-
-
-
-
-
-
-
-
-
-atomically  = undefined
-
-
-data Behaviour   a
-data Event       a
-data GUI
-data IVar        a
-data Input
-data Parallel    a
-data ReaderT r m a = ReaderT (r -> m a)
-data STM         a
-data Signal      a
-data TVar        a
-data Transform   a b
-data WriterT w m a = WriterT  (m (w, a))
 
 
 
