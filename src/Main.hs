d2e75e88217474fb59505a063d054eba2ae9ea07
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-02 23:40:26 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,14 @@
 module Main where
-import Data.IORef
-import Data.Time
-import qualified Data.Map as M
 
 
-type PopupTracker = IORef (M.Map User UTCTime)
 
-newPopupTracker :: IO PopupTracker
-newPopupTracker = newIORef M.empty
 
-recordPopup :: PopupTracker -> User -> UTCTime -> IO ()
-recordPopup ref u t = do
-  modifyIORef ref $ M.insert u t
-
-getLastPopup :: PopupTracker -> User -> IO (Maybe UTCTime)
-getLastPopup ref u = do
-  M.lookup u <$> readIORef ref
+--     ,----,                                    ,---,   
+--   /        \                                /       \ 
+--  |   Page   |   ----- recordPopup ----->   |  Popup  |
+--  | Renderer |   ----- getLastPopup ---->   | Tracker |
+--   \        /                                \       / 
+--     '----'                                    '---'   
 
 
 
@@ -28,9 +21,6 @@ getLastPopup ref u = do
 
 
 
-
-
-data User = User deriving (Eq, Ord)
 
 
 
