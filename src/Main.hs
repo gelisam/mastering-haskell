3353bbe58396f5bdba8831456483de0a04fc48c8
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-04 23:25:41 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,42 +1,34 @@
 module Main where
-
--- machine 1
-acquire1 :: IO ()
-process1 :: IO ()
-archive1 :: IO ()
-
--- machine 2
-process2 :: IO ()
-process3 :: IO ()
-process4 :: IO ()
-
--- machine 3
-process5 :: IO ()
-process6 :: IO ()
-process7 :: IO ()
-
-
-
-
-
-
-
-
-
-
-acquire1 = undefined
-process1 = undefined
-archive1 = undefined
-
-process2 = undefined
-process3 = undefined
-process4 = undefined
-
-process5 = undefined
-process6 = undefined
-process7 = undefined
-
-
+import Control.Concurrent.Async
 
 main :: IO ()
-main = return ()
+main = do
+  t1 <- async acquire
+  t2 <- async process
+  t3 <- async archive
+  mapM_ wait [t1,t2,t3]
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+acquire :: IO ()
+acquire = return ()
+
+process :: IO ()
+process = return ()
+
+archive :: IO ()
+archive = return ()
