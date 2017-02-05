1b28cdc79be4c9e29509fda01ee246d94a90e911
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-04 21:37:25 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -4,22 +4,22 @@ import Data.List (partition, sort)
 
 data SortingContainer a = forall s. SortingContainer
   { empty  :: s
-  , insert :: s -> a -> s  --  <-- API change
+  , insert :: s -> a -> s
   , sorted :: s -> [a]
   }
 
 progressiveImpl :: Ord a => SortingContainer a
 progressiveImpl = SortingContainer
   { empty = []
-  , insert = \x xs -> let (xsLT, xsGEQ) = partition (< x) xs  -- <-.
-                       in xsLT ++ [x] ++ xsGEQ  -- type error -----'
+  , insert = \xs x -> let (xsLT, xsGEQ) = partition (< x) xs
+                       in xsLT ++ [x] ++ xsGEQ
   , sorted = id
   }
 
 justInTimeImpl :: Ord a => SortingContainer a
 justInTimeImpl = SortingContainer
   { empty = []
-  , insert = (:)  --  <-- type error
+  , insert = flip (:)
   , sorted = sort
   }
 
