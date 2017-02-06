8782c3485138530c1436d44412241802f47e2718
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-06 15:06:16 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,51 @@ module Main where
 import Data.Hashable
 import Haxl.Core
 
+haxlComputation :: GenHaxl () Bool
+haxlComputation =
+  bool <$> (or <$> sequence [ dataFetch (PopupRequest 1)
+                            , dataFetch (PopupRequest 2)
+                            , dataFetch (PopupRequest 3)
+                            ])
+       <*> (and <$> sequence [ dataFetch (PopupRequest 10)
+                             , dataFetch (PopupRequest 20)
+                             , dataFetch (PopupRequest 30)
+                             ])
+       <*> (and <$> sequence [ dataFetch (PopupRequest 100)
+                             , dataFetch (PopupRequest 200)
+                             , dataFetch (PopupRequest 300)
+                             ])
+
+bool :: Bool -> a -> a -> a
+bool True  t _ = t
+bool False _ f = f
+
+
+
+
+
+
+delayedVisitCount :: Int -> IO Int
+delayedVisitCount days = do
+  putStrLn $ "delayedVisitCount " ++ show days
+  return 0
+
+hasDisplayedPopup :: Int -> IO Bool
+hasDisplayedPopup days = do
+  putStrLn $ "hasDisplayedPopup " ++ show days
+  return False
+
+filterPopupDays   :: [Int] -> IO [Int]
+filterPopupDays days = do
+  putStrLn $ "filterPopupDays " ++ show days
+  return []
+
+
+
+data Req a where
+  VisitRequest :: Int -> Req Int
+  PopupRequest :: Int -> Req Bool
+
 
 instance DataSource () Req where
   fetch _ _ _ reqs = SyncFetch $ do
@@ -17,46 +62,6 @@ instance DataSource () Req where
       popupDay :: BlockedFetch Req -> [Int]
       popupDay (BlockedFetch (PopupRequest day) _) = [day]
       popupDay _                                   = []
-
-
-
-delayedVisitCount :: Int -> IO Int
-hasDisplayedPopup :: Int -> IO Bool
-filterPopupDays   :: [Int] -> IO [Int]
-
-
-delayedVisitCount days = do
-  putStrLn $ "delayedVisitCount " ++ show days
-  return 0
-
-hasDisplayedPopup days = do
-  putStrLn $ "hasDisplayedPopup " ++ show days
-  return False
-
-filterPopupDays days = do
-  putStrLn $ "filterPopupDays " ++ show days
-  return []
-
-
-
-haxlSignal :: Signal a -> GenHaxl () a
-haxlSignal = go 0
-  where
-    go :: Int -> Signal a -> GenHaxl () a
-    go _     (Pure x)   = return x
-    go delay (Ap cc sx) = go delay cc <*> goF delay sx
-    
-    goF :: Int -> SignalF a -> GenHaxl () a
-    goF delay VisitCount          = dataFetch (VisitRequest delay)
-    goF delay HasDisplayedPopup   = dataFetch (PopupRequest delay)
-    goF delay (TimeDelayed d sx)  = go (delay + d) sx
-
-
-
-data Req a where
-  VisitRequest :: Int -> Req Int
-  PopupRequest :: Int -> Req Bool
-
 
 instance DataSourceName Req where
   dataSourceName _ = "Req"
@@ -87,44 +92,6 @@ runRequest (PopupRequest days) = hasDisplayedPopup days
 
 
 
-nDaysCount :: Int -> Signal Int
-nDaysCount days = (-) <$> visitCount <*> timeDelayed days visitCount
-
-veryActiveWeek :: Signal Bool
-veryActiveWeek = (>) <$> nDaysCount 7
-                     <*> ((-) <$> nDaysCount 365 <*> nDaysCount 7)
-
-
-
--- ((f <$> fx) <*> fy) <*> fz
-data FreeAp f a where
-  Pure :: a -> FreeAp f a
-  Ap   :: FreeAp f (e -> a) -> f e -> FreeAp f a
-
-type Signal a = FreeAp SignalF a
-data SignalF a where
-  VisitCount        :: SignalF Int
-  HasDisplayedPopup :: SignalF Bool
-  TimeDelayed       :: Int -> Signal a -> SignalF a
-
-visitCount :: Signal Int
-visitCount = Ap (Pure id) VisitCount
-
-timeDelayed :: Int -> Signal a -> Signal a
-timeDelayed days bx = Ap (Pure id) (TimeDelayed days bx)
-
-
-instance Functor (FreeAp f) where
-  fmap f (Pure x)   = Pure (f x)
-  fmap f (Ap fs fe) = Ap (fmap (fmap f) fs) fe
-
-instance Applicative (FreeAp f) where
-  pure = Pure
-  Pure f   <*> fx = fmap f fx
-  Ap fs fe <*> fx = Ap (flip <$> fs <*> fx) fe
-
-
-
 forM_ :: [a] -> (a -> IO ()) -> IO ()
 forM_ = flip mapM_
 
@@ -136,5 +103,5 @@ initialState = stateSet NoState stateEmpty
 main :: IO ()
 main = do
   myEnv <- initEnv initialState ()
-  _ <- runHaxl myEnv (haxlSignal veryActiveWeek)
+  _ <- runHaxl myEnv haxlComputation
   return ()
