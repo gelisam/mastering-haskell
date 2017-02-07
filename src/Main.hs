445cbf48b657bcf6ce9d2bd2039b95376e67fbbb
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-07 12:08:49 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,9 @@
-{-# LANGUAGE GADTs #-}
 module Main where
-import Network
-import System.IO
 
 
-
-
-
-
-rpcMyMethod :: Either String Int -> Int -> Int -> IO ()
-rpcMyMethod e x y = do
-  h <- connectTo host port
-  case e of
-    Left  s -> do hPutStrLn h $ serialize False
-                  hPutStrLn h $ serialize s
-                  hPutStrLn h $ serialize (0 :: Int)
-    Right i -> do hPutStrLn h $ serialize True
-                  hPutStrLn h $ serialize ""
-                  hPutStrLn h $ serialize i
-  hPutStrLn h $ serialize x
-  hPutStrLn h $ serialize y
+acquire :: IO ()
+process :: IO ()
+archive :: IO ()
 
 
 
@@ -31,29 +15,19 @@ rpcMyMethod e x y = do
 
 
 
-host :: HostName
-host = "localhost"
-
-port :: PortID
-port = PortNumber 1234
 
 
-class Serializable a where
-  serialize   :: a -> String
-  deserialize :: String -> a
 
 
-instance Serializable Bool where
-  serialize   = show
-  deserialize = read
 
-instance Serializable Int where
-  serialize   = show
-  deserialize = read
 
-instance (Show a, Read a) => Serializable [a] where
-  serialize   = show
-  deserialize = read
+
+
+
+acquire = undefined
+process = undefined
+archive = undefined
+
 
 
 main :: IO ()
