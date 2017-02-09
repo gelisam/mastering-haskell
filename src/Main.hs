ce1c539ebe8ebc64e565535ba6e9d1c6d6166613
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-09 09:09:14 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,33 +2,35 @@
 module Main where
 import Control.Distributed.Process.Closure
 import Control.Distributed.Process.Node (initRemoteTable)
-import Control.Distributed.Static hiding (initRemoteTable)
-import Data.Typeable
+import Control.Distributed.Static hiding (initRemoteTable)                                         -- Definitions must be before remotable.
+import Data.Typeable                                                                               -- uAp depends on eval which depends on remoteTable,
+                                                                                                   -- so we use closureApply in order to avoid a cycle.
+uAp       :: (Closure (a -> b), Closure a) -> b                                                    ; uAp (cF, cX) = undefined cF cX  -- (eval cF) (eval cX)
+uString   :: (String) -> String                                                                    ; uString      = id
+uReverse  :: ()       -> String -> String                                                          ; uReverse  () = reverse
+uPutStrLn :: ()       -> String -> IO ()                                                           ; uPutStrLn () = putStrLn
+                                                                                                   ; remotable [{-'uAp,-} 'uString, 'uReverse, 'uPutStrLn]
+cAp       :: Closure (a -> b) -> Closure a -> Closure b                                            ; cAp cF cX = closureApply cF cX  -- $(mkClosure 'uAp) (cF, cX)
+cString   :: String   -> Closure String                                                            ; cString   = $(mkClosure 'uString)
+cReverse  ::             Closure (String -> String)                                                ; cReverse  = $(mkClosure 'uReverse) ()
+cPutStrLn ::             Closure (String -> IO ())                                                 ; cPutStrLn = $(mkClosure 'uPutStrLn) ()
 
-greet :: (String, String) -> IO ()
-greet (greeting, name) = putStrLn $ greeting ++ " " ++ name ++ "!"
+infixl 4 <@>
+(<@>) :: Closure (a -> b) -> Closure a -> Closure b
+(<@>) = cAp
 
-remotable ['greet]
+main :: IO ()
+main = eval $ cPutStrLn <@> (cReverse <@> cString "hello")
 
-cGreet :: (String, String) -> Closure (IO ())
-cGreet = $(mkClosure 'greet)
+
+
+
+
 
 eval :: Typeable a => Closure a -> a
 eval cX = case unclosure remoteTable cX of
   Left  e -> error e
   Right x -> x
-
-main :: IO ()
-main = eval $ cGreet ("hello", "world")
-
-
-
-
-
-
-
-
-
 
 remoteTable :: RemoteTable
 remoteTable = __remoteTable initRemoteTable
