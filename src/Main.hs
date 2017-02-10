eb8774cb950db30561ae8fabb71fa2d9d8ad479d
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-10 10:53:17 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -7,31 +7,10 @@ import Control.Distributed.Process.Node (initRemoteTable)                       
 import Control.Distributed.Static hiding (initRemoteTable)                                         ; import System.Environment
 import Data.IORef                                                                                  ; sleep :: Double -> IO (); sleep seconds = threadDelay $ round $ seconds * 1000 * 1000; forever :: Monad m => m () -> m (); forever body = body >> forever body; serializableDict_Unit :: SerializableDict (); serializableDict_Unit = SerializableDict; uInt :: Int -> Int; uInt = id; uPid :: ProcessId -> ProcessId; uPid = id; uUndefined :: () -> a; uUndefined () = undefined; uReturn :: () -> a -> Process a; uReturn () = return; class CSend a where { cSend :: Closure (ProcessId -> a -> Process ()) }; uSend_ProcessId_Maybe_Int :: () -> ProcessId -> (ProcessId, Maybe Int) -> Process (); uSend_ProcessId_Maybe_Int () = send; uRunThenSendBack :: forall a. () -> (ProcessId -> (ProcessId, Maybe a) -> Process ()) -> Process a -> ProcessId -> Process (); uRunThenSendBack () send_ body p = let sendBack :: Maybe a -> Process (); sendBack maybeX = do { q <- getSelfPid; send_ p (q, maybeX) } in onException (sendBack . Just =<< body) (sendBack Nothing); remotable ['serializableDict_Unit, 'uInt, 'uPid, 'uUndefined, 'uReturn, 'uSend_ProcessId_Maybe_Int, 'uRunThenSendBack]; cInt :: Int -> Closure Int; cInt = $(mkClosure 'uInt); cPid :: ProcessId -> Closure ProcessId; cPid = $(mkClosure 'uPid); uUndefined__sdict :: Static (SerializableDict ()); uUndefined__sdict = $(mkStatic 'serializableDict_Unit); cUndefined :: Typeable a => Closure a; cUndefined = $(mkClosure 'uUndefined) (); uReturn__sdict :: Static (SerializableDict ()); uReturn__sdict = $(mkStatic 'serializableDict_Unit); cReturn :: Typeable a => Closure (a -> Process a); cReturn = $(mkClosure 'uReturn) (); instance CSend (ProcessId, Maybe Int) where { cSend = $(mkClosure 'uSend_ProcessId_Maybe_Int) () }; uRunThenSendBack__sdict :: Static (SerializableDict ()); uRunThenSendBack__sdict = $(mkStatic 'serializableDict_Unit); cRunThenSendBack :: (Typeable a, CSend (ProcessId, Maybe a)) => Closure (Process a -> ProcessId -> Process ()); cRunThenSendBack = $(mkClosure 'uRunThenSendBack) () <@> cSend
 
-distributedMain :: NodeId -> [NodeId] -> Process ()                                                ; distributedMain _ [] = error "expected at least one node"
-distributedMain _ (node:_) = do
-  forM_ [cReturn <@> cInt 42, cUndefined] $ \cBody -> do
-    asyncX <- async node cBody
-    
-    liftIO $ putStrLn "waiting"
-    liftIO . print =<< wait asyncX
-    liftIO $ putStrLn "waited"
-
-spawn' :: NodeId -> Closure (Process ()) -> Process ()
+type Async a = IORef (Either (Maybe a) ProcessId)
 
 async :: Serializable a                                                                            => CSend (ProcessId, Maybe a)
       => NodeId -> Closure (Process a) -> Process (Async a)
-wait  :: Serializable a
-      => Async a -> Process a
-
-
-
-
-
-
-
-
-type Async a = IORef (Either (Maybe a) ProcessId)
-
 async node cBody = do
   p <- getSelfPid
   q <- spawn node (cRunThenSendBack <@> cBody <@> cPid p)
@@ -45,6 +24,10 @@ runThenSendBack body p = onException (sendBack . Just =<< body)
         sendBack maybeX = do q <- getSelfPid
                              send p (q, maybeX)
 
+
+
+wait :: Serializable a
+     => Async a -> Process a
 wait ref = liftIO (readIORef ref) >>= \case
   Left Nothing  -> error "some remote exception"
   Left (Just x) -> return x
@@ -54,6 +37,18 @@ wait ref = liftIO (readIORef ref) >>= \case
 
 
 
+distributedMain :: NodeId -> [NodeId] -> Process ()                                                ; distributedMain _ [] = error "expected at least one node"
+distributedMain _ (node:_) = do
+  forM_ [cReturn <@> cInt 42, cUndefined] $ \cBody -> do
+    asyncX <- async node cBody
+    
+    liftIO $ putStrLn "waiting"
+    liftIO . print =<< wait asyncX
+    liftIO $ putStrLn "waited"
+
+
+
+spawn' :: NodeId -> Closure (Process ()) -> Process ()
 spawn' node cBody = void $ spawn node cBody
 
 infixl 4 <@>
