27667c8a989f679a6935246883b094ef4fc6ccd6
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-10 10:35:27 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,28 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ScopedTypeVariables, TemplateHaskell #-}                                              {-# LANGUAGE DeriveGeneric, StandaloneDeriving #-}
 module Main where
 import Control.Distributed.Process
-import Control.Distributed.Process.Backend.SimpleLocalnet
-import Control.Distributed.Process.Closure
-import Control.Distributed.Process.Node (initRemoteTable)                                          ; import Control.Concurrent
-import Control.Distributed.Static hiding (initRemoteTable)                                         ; import System.Environment
-import Data.IORef                                                                                  ; sleep :: Double -> IO (); sleep seconds = threadDelay $ round $ seconds * 1000 * 1000; uIORef :: IORef Int -> IORef Int; uIORef = id; uContinuouslyUpdate :: () -> IORef Int -> Process (); uContinuouslyUpdate () ref = do { liftIO $ modifyIORef ref (+1); liftIO $ sleep 0.5; uContinuouslyUpdate () ref }; remotable ['uIORef, 'uContinuouslyUpdate]; cIORef :: IORef Int -> Closure (IORef Int); cIORef = $(mkClosure 'uIORef); cContinuouslyUpdate :: Closure (IORef Int -> Process ()); cContinuouslyUpdate = $(mkClosure 'uContinuouslyUpdate) ()
+import Control.Distributed.Process.Backend.SimpleLocalnet                                          ; import Control.Concurrent
+import Control.Distributed.Process.Closure                                                         ; import Data.Binary
+import Control.Distributed.Process.Node (initRemoteTable)                                          ; import Data.Typeable
+import Control.Distributed.Static hiding (initRemoteTable)                                         ; import GHC.Generics
+import Data.IORef                                                                                  ; import System.Environment
 
-distributedMain :: NodeId -> [NodeId] -> Process ()
-distributedMain thisNode otherNodes = do
-  ref <- liftIO $ newIORef 0
-  forM_ otherNodes $ \node -> do
-    spawn' node $ cContinuouslyUpdate <@> cIORef ref
-                                        -- IORef not serializable
+data Msg = Update (Closure (Int -> Int)) ProcessId                                                 ; deriving instance Generic Msg; instance Binary Msg; sleep :: Double -> IO (); sleep seconds = threadDelay $ round $ seconds * 1000 * 1000; forever :: Monad m => m () -> m (); forever body = body >> forever body; eval :: Typeable a => Closure a -> a; eval = undefined; remotableDecl [ [d| uAdd :: () -> Int -> Int -> Int; uAdd () = (+); |], [d| uInt :: Int -> Int; uInt = id |], [d| uStatefulProcess :: () -> Process (); uStatefulProcess = let eval_ :: Typeable a => Closure a -> a; eval_ cX = case unclosure remoteTable_ cX of { Left e -> error e; Right x -> x }; remoteTable_ :: RemoteTable; remoteTable_ = __remoteTableDecl $ initRemoteTable in \() -> do { ref <- liftIO $ newIORef (0 :: Int); forever $ receiveWait [ match $ \(Update f p) -> do { liftIO $ modifyIORef ref (eval_ f); send p =<< liftIO (readIORef ref) } ] } |] ]; cAdd :: Closure (Int -> Int -> Int); cAdd = $(mkClosure 'uAdd) (); cInt :: Int -> Closure Int; cInt = $(mkClosure 'uInt); cStatefulProcess :: Closure (Process ()); cStatefulProcess = $(mkClosure 'uStatefulProcess) ()
 
+distributedMain :: NodeId -> [NodeId] -> Process ()                                                ; distributedMain _ [] = error "expected at least one node"
+distributedMain _ (node:_) = do
+  p <- getSelfPid
+  q <- spawn node cStatefulProcess
+  forever $ do send q (Update (cAdd <@> cInt 1) p)
+               receiveWait [match $ \(r :: Int) -> liftIO $ print r]
+               liftIO $ sleep 0.5
 
-
-
-continuouslyUpdate :: IORef Int -> Process ()
-continuouslyUpdate ref = do liftIO $ modifyIORef ref (+1)
-                            liftIO $ sleep 0.5
-                            continuouslyUpdate ref
-
-
-
+statefulProcess :: Process ()
+statefulProcess = do ref <- liftIO $ newIORef (0 :: Int)
+                     forever $ do
+                       receiveWait [match $ \(Update f p) -> do
+                         liftIO $ modifyIORef ref (eval f)
+                         send p =<< liftIO (readIORef ref) ]
 
 
 
@@ -35,7 +34,7 @@ infixl 4 <@>
 (<@>) = closureApply
 
 remoteTable :: RemoteTable
-remoteTable = __remoteTable
+remoteTable = __remoteTableDecl
             $ initRemoteTable
 
 
@@ -78,9 +77,6 @@ void body = do _ <- body
 when :: Monad m => Bool -> m () -> m ()
 when False _   = return ()
 when True body = body
-
-forever :: Monad m => m () -> m ()
-forever body = body >> forever body
 
 forM_ :: Monad m => [a] -> (a -> m ()) -> m ()
 forM_ = flip mapM_
