f2ee4b4929260b894430d3189f9fef375857a2aa
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-12 17:20:32 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -6,17 +6,30 @@ import Data.Set as Set
 class Semigroup s => CRDT s a where
   value :: s -> a
 
+instance CRDT () () where
+  value () = ()
+
+instance (CRDT s a, CRDT t b) => CRDT (s,t) (a,b) where
+  value (s,t) = (value s, value t)
+
+
+
+
+
+main :: IO ()
+main = do
+  let x1 = (tt, (ff, (ff, ())))
+      x2 = (ff, (tt, (ff, ())))
+  print (value (x1 <> x2) :: (Bool, (Bool, (Bool, ()))))
+
+
+
 newtype PermanentFlag = PermanentFlag (Set ())
-  deriving (Eq, Semigroup)
+  deriving Semigroup
 
 instance CRDT PermanentFlag Bool where
-  value = (== tt)
+  value (PermanentFlag s) = s == Set.singleton ()
 
 ff, tt :: PermanentFlag
 ff = PermanentFlag Set.empty
 tt = PermanentFlag (Set.singleton ())
-
-main :: IO ()
-main = do
-  print (value (ff <> ff) :: Bool)
-  print (value (ff <> tt) :: Bool)
