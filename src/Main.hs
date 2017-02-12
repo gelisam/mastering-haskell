779f75524c54abb02cff9b82cd81ee235dc2eec0
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-12 10:43:34 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,17 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Main where
 import Data.Semigroup
 import Data.Set as Set
 
--- (<>) must be commutative and idempotent (and associative).
-class Semigroup s => CRDT s
-  -- (<>) :: s -> s -> Either Conflict s
+class Semigroup s => CRDT s a where
+  value :: s -> a
 
-instance Ord a => CRDT (Set a)
-
+instance Ord a => CRDT (Set a) (Set a) where
+  value = id
 
 main :: IO ()
 main = do
   let xs  = Set.fromList ["apple","banana"]
       xsL = Set.insert "pineapple" xs
       xsR = Set.insert "melon"     xs
-  print (xsL <> xsR)
+  print (value (xsL <> xsR) :: Set String)
