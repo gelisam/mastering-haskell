00b17b2726936fa61c0f7848f6ec97173b6b62cd
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-12 15:56:51 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,45 +1,45 @@
 {-# LANGUAGE LambdaCase #-}
 module Main where
+import Control.Applicative
 import Control.Monad.Trans.Maybe
 
+renderProductDetails :: ProductId -> Process HTML
+renderProductDetails p = runMaybeT go >>= \case
+    Just t  -> renderTemplate t
+    Nothing -> renderText "This page is currently unavailable."
+  where
+    go :: MaybeT Process (Template HTML)
+    go = productDetailsPage
+     <$> getName p
+     <*> (getImage p        <|> return "no-image-available.png")
+     <*> (getDesc  p        <|> return "no description available")
+     <*> (getCurrentPrice p <|> estimatePrice p)
+     <*> (renderInventory   <|> return emptyDiv)
+     <*> (renderReviews     <|> return emptyDiv)
+     <*> (renderRelated     <|> return emptyDiv)
+    
+    renderInventory :: MaybeT Process HTML
+    renderInventory = do
+      n <- getInventoryCount p
+      renderText $ "In stock! " ++ show n ++ " copies left."
+    
+    
+    
+    renderReviews :: MaybeT Process HTML
+    renderReviews = undefined
+    
+    renderRelated :: MaybeT Process HTML
+    renderRelated = undefined
+    
+    productDetailsPage :: String -> String -> String
+                       -> Price
+                       -> HTML -> HTML -> HTML
+                       -> Template HTML
+    productDetailsPage = undefined
 
-wait :: Async a -> MaybeT Process a
-wait asyncX = realWait asyncX >>= \case
-  Success   x -> return x
-  Exception e -> error e
-  LostContact -> empty
 
 
-attempt1 :: MaybeT Process Int
-attempt2 :: MaybeT Process Int
-
-tryYourBest :: MaybeT Process Int
-tryYourBest = attempt1 <|> attempt2 <|> return 0
-
-(<|>) :: MaybeT Process a -> MaybeT Process a -> MaybeT Process a
-
-
-
-
-
-
-
-MaybeT mMaybeX <|> MaybeT mMaybeX' = MaybeT $ do
-  mMaybeX >>= \case
-    Just x  -> return (Just x)
-    Nothing -> mMaybeX'
-  
-empty :: MaybeT Process a
-empty = MaybeT $ return Nothing
-
-attempt1 = undefined
-attempt2 = undefined
-
-
-
-data Async a
 data Process a
-data Result a = Success a | Exception String | LostContact
 
 instance Functor Process where
   fmap = undefined
@@ -51,8 +51,62 @@ instance Applicative Process where
 instance Monad Process where
   (>>=) = undefined
 
-realWait :: Async a -> MaybeT Process (Result a)
-realWait = undefined
+
+class Monad m => MonadProcess m where
+  liftProcess :: Process a -> m a
+
+instance MonadProcess Process where
+  liftProcess = id
+
+instance MonadProcess m => MonadProcess (MaybeT m) where
+  liftProcess px = MaybeT (Just <$> liftProcess px)
+
+
+
+data HTML
+
+emptyDiv :: HTML
+emptyDiv = undefined
+
+renderText :: MonadProcess m => String -> m HTML
+renderText = undefined
+
+
+data Template a
+
+instance Functor Template where
+  fmap = undefined
+
+instance Applicative Template where
+  pure  = undefined
+  (<*>) = undefined
+
+renderTemplate :: MonadProcess m => Template a -> m a
+renderTemplate = undefined
+
+
+data ProductId
+
+getName :: ProductId -> MaybeT Process String
+getName = undefined
+
+getImage :: ProductId -> MaybeT Process String
+getImage = undefined
+
+getDesc :: ProductId -> MaybeT Process String
+getDesc = undefined
+
+getInventoryCount :: ProductId -> MaybeT Process Int
+getInventoryCount = undefined
+
+
+data Price
+
+getCurrentPrice :: ProductId -> MaybeT Process Price
+getCurrentPrice = undefined
+
+estimatePrice :: ProductId -> MaybeT Process Price
+estimatePrice = undefined
 
 
 
